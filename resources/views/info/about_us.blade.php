b347e6154847be8487e8436ea98128171b34dcee
[sachintiwari809@gmail.com] <[sachintiwari809@gmail.com]>
2018-09-28 15:18:43 +0530

    all static and css changes

---
+++ resources/views/info/about_us.blade.php
@@ -0,0 +1,121 @@
+@extends('customer.layouts.master')
+@section('content')
+<div class="create-account--header">
+	<div class="wrapper">
+		<div class="row">
+			<div class="col-6">
+				<div class="about-header">
+					<h1 class="br-mainheading">About Us</h1>
+				    <p>Unfortunately running injuries are all too common so it is important to know ways in which you can best try to prevent them occuring. We list the common injuries that runners sustain and give you information on treatment available.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="accordian-container about-container">
+    <div class="accordian-tab color1">
+      <input type="checkbox" name="accordian" class="input" id="tab-1">
+      <label for="tab-1">
+	      	No matter the question, the answer is ALWAYS THE RUN
+      </label>
+      <div class="accordian-content">
+         <div class="contents-wrapper">
+     		<div class="about-container--info">
+     			<p>Whatever you’re doing, wherever you are, when you run you break free of things that hold you back. You discover your path to truth and joy.</p>
+     		</div>
+         </div>
+      </div>
+    </div>
+    <div class="accordian-tab color2">
+      <input type="checkbox" name="accordian" class="input" id="tab-2">
+      <label for="tab-2">
+	      	Running makes good things happen
+      </label>
+      <div class="accordian-content">
+         <div class="contents-wrapper">
+     		<div class="about-container--info">
+     			<p>It’s the best, most addictive sport the world has ever known. It fuels confidence and optimism. It makes you feel better about yourself. It can change everything.</p>
+     		</div>
+         </div>
+      </div>
+    </div>
+    <div class="accordian-tab color3">
+      <input type="checkbox" name="accordian" class="input" id="tab-3">
+      <label for="tab-3">
+	      	We do one thing at Brooks
+      </label>
+      <div class="accordian-content">
+         <div class="contents-wrapper">
+     		<div class="about-container--info">
+     			<p>We make the best running gear in the world. Nothing more. Nothing less. Every design and engineering choice is informed by runners’ needs and the running experiences they crave.</p>
+     		</div>
+         </div>
+      </div>
+    </div>
+    <div class="accordian-tab color4">
+      <input type="checkbox" name="accordian" class="input" id="tab-4">
+      <label for="tab-4">
+	      	We're experts at balancing art, science and runner insight
+      </label>
+      <div class="accordian-content">
+         <div class="contents-wrapper">
+     		<div class="about-container--info">
+     			<p>Backed by in-depth biomechanical research, the products we make for you feature innovations that are always advancing the fit, feel, ride, style and performance of your running gear.</p>
+     		</div>
+         </div>
+      </div>
+    </div>
+    <div class="accordian-tab color1">
+      <input type="checkbox" name="accordian" class="input" id="tab-5">
+      <label for="tab-5">
+	      	We sweat every last detail
+      </label>
+      <div class="accordian-content">
+         <div class="contents-wrapper">
+     		<div class="about-container--info">
+     			<p>You might never notice each precisely tuned groove on the sole of your shoe or the high-tech weave that makes your jacket so breathable. But it’s those hundreds of little things that make your gear as exceptional as you are. You deserve it all.</p>
+     		</div>
+         </div>
+      </div>
+    </div>
+    <div class="accordian-tab color2">
+      <input type="checkbox" name="accordian" class="input" id="tab-6">
+      <label for="tab-6">
+	      	We get you
+      </label>
+      <div class="accordian-content">
+         <div class="contents-wrapper">
+     		<div class="about-container--info">
+     			<p>We’re runners like you, and we’re insatiably curious about your needs, your dreams, your commitment to the run, your fitness and your well-being. We see the run as you do: it makes you feel more alive.</p>
+     		</div>
+         </div>
+      </div>
+    </div>
+    <div class="accordian-tab color3">
+      <input type="checkbox" name="accordian" class="input" id="tab-7">
+      <label for="tab-7">
+	      	We live, work and run as part of a global community
+      </label>
+      <div class="accordian-content">
+         <div class="contents-wrapper">
+     		<div class="about-container--info">
+     			<p>This planet is our playground. More than 100 million people run outside, so it’s critical we care for the world we share. We also give back with a focus on causes that get people moving.</p>
+     		</div>
+         </div>
+      </div>
+    </div>
+    <div class="accordian-tab color4">
+      <input type="checkbox" name="accordian" class="input" id="tab-8">
+      <label for="tab-8">
+	      	Every day with a run is better
+      </label>
+      <div class="accordian-content">
+         <div class="contents-wrapper">
+     		<div class="about-container--info">
+     			<p>No matter your speed, style or where you start, you end up in a happier place when you run. That happy place is our North Star at Brooks. It guides everything we do to inspire you to run and be active.</p>
+     		</div>
+         </div>
+      </div>
+    </div>
+</div>
+@endsection
