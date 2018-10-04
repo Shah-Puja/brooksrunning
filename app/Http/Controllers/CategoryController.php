8b191a1501074442eb28b5f092e75b65ba74a4f4
purvishah <purvi.cshah@gmail.com>
2018-10-04 14:54:37 +0530

    category page

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,6 +7,28 @@ use Illuminate\Http\Request;
 class CategoryController extends Controller
 {
     public function index($category) {
+        $cat_info= \App\Models\Category::where('slug',$category)->first();
+        if ($cat_info->count() < 1) {
+            abort(404);
+        }
+        else{
+            $gender = $cat_info->gender;            
+            $prod_type = "Footwear"; //$cat_info->prod_type;
+            $products = \App\Models\Category::getProducts($category);
+            if ($products->count() < 1) {
+                echo "<br> No Products";
+                $styles=$flag_bra="";              
+                $filters=[];
+                //exit;  
+            }
+            else{
+                $styles = $products->unique('style');                                
+                $filters = \App\Models\Category::provideFilters($products,$prod_type);                
+            }            
+            return view( 'customer.categorylower', compact('products', 'styles','filters','prod_type','gender','flag_bra') );                
+        }
+    }
+    public function index_trunal($category) {
 
         $products = \App\Models\Category::getProducts($category);
 
