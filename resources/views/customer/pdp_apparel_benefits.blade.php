0ca6c7ef52d6dfaab2976d822b4d8b01cd817bcc
trunalt <trunaltamore@gmail.com>
2018-10-09 15:13:26 +0530

    PDP changes

--- resources/views/customer/pdp_apparel_benefits.blade.php
+++ resources/views/customer/pdp_apparel_benefits.blade.php
@@ -1,3 +1,7 @@
+@php  $benefit_1 = (!empty($product->benefit_1)) ? explode('#', $product->benefit_1) : ''; @endphp
+@php  $benefit_2 = (!empty($product->benefit_2)) ? explode('#', $product->benefit_2) : ''; @endphp
+@php  $benefit_3 = (!empty($product->benefit_3)) ? explode('#', $product->benefit_3) : ''; @endphp
+@if($benefit_1)
 <section class="pdp-container--benefits">
 	<div class="wrapper module-header">
 		<div class="col-12">
@@ -11,14 +15,17 @@
 			<div class="col-6">
 				<div class="info-wrapper clearfix">
 					<div class="info right">
-						<h3 class="br-heading">Odor-Resistant</h3>
-						<p class="br-info">The inherently odor-resistant nature of DriLayer® Burst fabric is designed to do away with odors day in and day out.</p>
+					 	<h3 class="br-heading">{{ ($benefit_1!='') ? $benefit_1[1] : "" }}</h3>
+						<p class="br-info">{{ ($benefit_1!='') ? $benefit_1[2] : "" }}</p>
 					</div>
 			    </div>
 			</div>
 			<div class="col-6">
 				<div class="img">
-					<img alt="Protective Stability" src="/images/details/apparel1.png" alt="">
+				    <picture>
+						<source media="(max-width: 595px)" srcset="/images/details/apparel1.png">
+						<img src="/images/details/apparel1.png" alt="Header Images">
+					</picture>
 				</div>
 			</div>
 		</div>
@@ -28,21 +35,24 @@
 			<div class="col-6 visible-mob visible-tab">
 				<div class="info-wrapper clearfix">
 					<div class="info">
-						<h3 class="br-heading">Comfortable Design</h3>
-						<p class="br-info">A building block for run or workout, it's designed and constructed for comfort so you can take your mind off what you're wearing and focus on what's ahead of you.</p>
+						<h3 class="br-heading">{{ ($benefit_2!='') ? $benefit_2[1] : "" }}</h3>
+						<p class="br-info">{{ ($benefit_2!='') ? $benefit_2[2] : "" }}</p>
 					</div>
 			    </div>
 			</div>
 			<div class="col-6">
 				<div class="img">
-					<img alt="Protective Stability" src="/images/details/apparel2.png" alt="">
+					<picture>
+						<source media="(max-width: 595px)" srcset="/images/details/apparel2.png">
+						<img src="/images/details/apparel2.png" alt="Header Images">
+					</picture>
 				</div>
 			</div>
 			<div class="col-6 hidden-mob hidden-tab">
 				<div class="info-wrapper clearfix">
 					<div class="info">
-						<h3 class="br-heading">Comfortable Design</h3>
-						<p class="br-info">A building block for run or workout, it's designed and constructed for comfort so you can take your mind off what you're wearing and focus on what's ahead of you.</p>
+						<h3 class="br-heading">{{ ($benefit_2!='') ? $benefit_2[1] : "" }}</h3>
+						<p class="br-info">{{ ($benefit_2!='') ? $benefit_2[2] : "" }}</p>
 					</div>
 			    </div>
 			</div>
@@ -53,36 +63,40 @@
 			<div class="col-6">
 				<div class="info-wrapper clearfix">
 					<div class="info right">
-						<h3 class="br-heading">Dries Quickly</h3>
-						<p class="br-info">Just because you've earned the sweat doesn't mean you want to run in it. Our DriLayer® fabric wicks sweat away to keep you comfortable, dry and feeling light on your feet.</p>
+						<h3 class="br-heading">{{ ($benefit_3!='') ? $benefit_3[1] : "" }}</h3>
+						<p class="br-info">{{ ($benefit_3!='') ? $benefit_3[2] : "" }}</p>
 					</div>
 			    </div>
 			</div>
 			<div class="col-6">
 				<div class="img">
-					<img alt="Rugged Traction" src="/images/details/apparel3.png">
+					<picture>
+						<source media="(max-width: 595px)" srcset="/images/details/apparel3.png">
+						<img src="/images/details/apparel3.png" alt="Header Images">
+					</picture>
 				</div>
 			</div>
 		</div>
 	</div>
 </section>
+@endif
 
-								<!-- Size chart popup  -->
-								<div id="sizechart-popup--wrapper" class="popup-container size-chart--popup">
-									<div class="popup-container--wrapper">
-										<div class="popup-container--info">
-											<div class="close-me" id="sizechart-popup--close"><span class="icon-close-icon"></span></div>
-											<!-- clothing size chart -->
-											<div class="size-chart--bras">
-												<h3 class="br-heading">Men's Clothing</h3>
-												<div class="info">
-													<h4>Size &amp; Fit Guide</h4>
-													<p>Sizing on BrooksRunning.com is displayed in US sizes only.</p>
-												</div>
-												<img src="/images/size-chart--clothings.png" alt="">
-											</div>
-											<!-- /clothing size chart -->
-										</div>
-									</div>
-								</div>
-								<!-- /Size chart popup -->
+<!-- Size chart popup  -->
+<div id="sizechart-popup--wrapper" class="popup-container size-chart--popup">
+	<div class="popup-container--wrapper">
+		<div class="popup-container--info">
+			<div class="close-me" id="sizechart-popup--close"><span class="icon-close-icon"></span></div>
+			<!-- clothing size chart -->
+			<div class="size-chart--bras">
+				<h3 class="br-heading">Men's Clothing</h3>
+				<div class="info">
+					<h4>Size &amp; Fit Guide</h4>
+					<p>Sizing on BrooksRunning.com is displayed in US sizes only.</p>
+				</div>
+				<img src="/images/size-chart--clothings.png" alt="">
+			</div>
+			<!-- /clothing size chart -->
+		</div>
+	</div>
+</div>
+<!-- /Size chart popup -->
