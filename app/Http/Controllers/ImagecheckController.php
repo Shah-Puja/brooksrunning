58313b02a2d9915bd77612b61b22875c22e2274a
trunalt <trunaltamore@gmail.com>
2018-10-17 15:02:55 +0530

    Image check

--- app/Http/Controllers/ImagecheckController.php
+++ app/Http/Controllers/ImagecheckController.php
@@ -4,11 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Imagecheck;
 
 class ImagecheckController extends Controller
 {
     public function index(){
-        $files = Storage::disk('sftp')->files('public_html/product/orig/');
-        print_r($files);
+        $files = Storage::disk('sftp')->files('public_html/product/t/');
+        $records = [];
+        foreach($files as $file){
+            $record = [
+                'image' => $file,
+            ];
+            $records[] = $record;
+        }
+        Imagecheck::insert($records);
     }
 }
