4865cc6ba9388c7a711d98105cd21a471c564e05
trunalt <trunaltamore@gmail.com>
2018-10-25 15:14:19 +0530

    order api

--- app/Models/Order_log.php
+++ app/Models/Order_log.php
@@ -9,7 +9,7 @@ class Order_log extends Model
     protected $table = 'order_log';
     protected $fillable = ['order_id','log_title'];
 
-    public static function createNew($order_id, $log_title, $log_type='', $log_status='', $result='', $xml = '', $nab_txnid = '', $nab_result = ''){
+    public static function createNew($order_id, $log_title, $log_type, $log_status, $result, $xml , $nab_txnid , $nab_result){
         $logger = array(
             'order_id'      => $order_id,
             'log_title'     => $log_title,
