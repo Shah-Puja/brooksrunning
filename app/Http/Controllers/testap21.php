5cd53409b20a5f353950b7e323f6a8d58048b0a2
purvishah <purvi.cshah@gmail.com>
2018-10-26 10:58:53 +0530

    Update testap21.php

--- app/Http/Controllers/testap21.php
+++ app/Http/Controllers/testap21.php
@@ -17,7 +17,7 @@ class testap21 extends Controller
                         <Firstname>Test f</Firstname>
                         <Surname>test l</Surname>
                         <Contacts>
-                          <Email>zd@syginteractive.com</Email>
+                          <Email>ze@syginteractive.com</Email>
                           <Phones>
                             <Home>11111</Home>
                           </Phones>
