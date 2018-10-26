6a5afc3d969f68a7bd8ce591abc7aa7d5067be71
purvishah <purvi.cshah@gmail.com>
2018-10-26 11:35:15 +0530

    Update testap21.php

--- app/Http/Controllers/testap21.php
+++ app/Http/Controllers/testap21.php
@@ -15,7 +15,7 @@ public function create_order($person_id='115414'){
         $xml_data = "
         <Order>
         <PersonId>115414</PersonId>
-        <OrderNumber>BRN-2018-test-3</OrderNumber>
+        <OrderNumber>BRN-2018-test-4</OrderNumber>
         <DeliveryInstructions></DeliveryInstructions>
         <Addresses>
             <Billing>
@@ -69,7 +69,6 @@ public function create_order($person_id='115414'){
     
         $response = $this->bridge->processOrder($person_id,$xml_data);
         print_r($response);
-        exit;
     
         $returnCode =  $response->getStatusCode();
         switch ($returnCode) {
