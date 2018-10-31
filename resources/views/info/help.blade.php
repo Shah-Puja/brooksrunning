ab243893381cb2cf35a731e01e57622883adec63
[sachintiwari809@gmail.com] <[sachintiwari809@gmail.com]>
2018-10-31 17:13:32 +0530

    shoe-page-changes

--- resources/views/info/help.blade.php
+++ resources/views/info/help.blade.php
@@ -1,60 +1,216 @@
 @extends('customer.layouts.master')
 @section('content')
-<div class="create-account--header">
-	<div class="wrapper">
+
+<link rel="stylesheet" href="/css/main.css">
+<div class="pdp-container">
+	<div class="wrapper detail-shoe-pg">
 		<div class="row">
-			<div class="col-12">
-				<h1 class="br-mainheading">Title of the page</h1>
+			<div class="col-7">
+				<div class="breadcrumbs2 hidden-tab hidden-mob">
+						<ul>
+							<li>
+								<a href="#">Home</a>
+							</li>
+							<li>
+								<a href="#">Shoes</a>
+							</li>
+							<li>
+								<a href="#" class="active">Liberty</a>
+							</li>
+						</ul>
+					</div>
+					<div class="heading-wrapper clearfix">
+						<div class="heading">
+							<h1 class="br-heading-detail">Liberty</h1>
+						</div>
+					</div>
+				<div class="pdp-container--image">
+ 				<img src="/images/shoes/shoes3-detail.jpg" />
+				</div>
+			</div>
+			<div class="col-5">
+				<div class="pdp-container--info">
+					<div class="shoe-info-wrapper">
+						<p class="br-info">
+                                    Following its namesake, Brooks Liberty shoes are truly liberating. You'll feel free to take on whatever the day throws at you when you're strapped into these neutral X-trainers, with extra stability and cushioning ensuring you stay nice and comfortable throughout. If you're into sports, get your feet into a pair of Brooks Liberty running shoes. Available in both mesh and leather options. Check out our <a href="/liberty-9-leather-mens-cross-training-shoes/110220_043.html">Shop Now Liberty 9</a> and <a href="/liberty-9-leather-womens-cross-training-shoes/120211_571.html">woShop Now Liberty 9</a> running shoes today, or find other fantastic options within our range of <a href="/mens-cross-training-shoes">men’s cross trainers</a> and <a href="/womens-cross-training-shoes">cross training running shoes for women</a>.                                
+                         </p>
+					</div>
+				</div>
+				
 			</div>
 		</div>
 	</div>
 </div>
-<section class="create-account wrapper">
-	<div class="row">
-		<div class="col-9">
-			<div class="create-account--left contact-us--container">
-				<h3 class="br-heading">Main Heading</h3>
-
-				<p class="br-info">Body Copy Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat </p>
-
-				<p class="br-info"><span class="bold">Sub heading</span> <br/>non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
-
-				<ul class="br-info">
-					<li>Bullet point</li>
-					<li>Bullet point</li>
-					<li>Bullet point</li>
-					<li>Bullet point</li>
-					<li>Bullet point</li>
-					<li>Bullet point</li>
-					<li>Bullet point</li>
-				</ul>
-
-				<p class="br-info"><a href="#" class="link">Link Text</a></p>
-			</div>
+<div class="ctg-main-container ">
+<section class="ctg-container ">
+	<div class="wrapper">
+		<div class="heading">
+					<h3 class="br-shoe-heading">Shop All Liberty Cross Training Shoes</h3>
+				</div>
+		<div class="row detail-shoe-pg">
+			<div class="ctg-wrapper-container">
+				<div class="mob-6 tab-4 col-4">
+					<div class="shoe-wrapper__sub">
+						<div class="ctg-product">
+							<div class="offer-info">
+								<img src="/images/category/updated/runsig_icon_stacked_cushion.svg" alt="" width="100" height="100">
+							</div>
+							<div class="img img-shoes">
+								<img id="plp-img" src="/images/category/placeholder_shoe category.png" alt="">
+							</div>						
+							<div class="info">
+								<h3>PureFlow</h3>
+								<div class="price">$179.95</div>
+								<div class="ctg-btn clearfix">
+									<span><a class="secondary-button" href="#">Shop Now</a></span>
+								</div>
+							</div>
+						</div>
+				   </div>
+			   </div>
+			   <div class="mob-6 tab-4 col-4">
+					<div class="shoe-wrapper__sub">
+						<div class="ctg-product">
+							<div class="offer-info">
+								<img src="/images/category/updated/runsig_icon_stacked_cushion.svg" alt="" width="100" height="100">
+							</div>
+							<div class="img img-shoes">
+								<img id="plp-img" src="/images/category/placeholder_shoe category.png" alt="">
+							</div>						
+							<div class="info">
+								<h3>PureFlow</h3>
+								<div class="price">$179.95</div>
+								<div class="ctg-btn clearfix">
+									<span><a class="secondary-button" href="#">Shop Now</a></span>
+								</div>
+							</div>
+						</div>
+				   </div>
+			   </div>
+			   <div class="mob-6 tab-4 col-4">
+					<div class="shoe-wrapper__sub">
+						<div class="ctg-product">
+							<div class="offer-info">
+								<img src="/images/category/updated/runsig_icon_stacked_cushion.svg" alt="" width="100" height="100">
+							</div>
+							<div class="img img-shoes">
+								<img id="plp-img" src="/images/category/placeholder_shoe category.png" alt="">
+							</div>						
+							<div class="info">
+								<h3>PureFlow</h3>
+								<div class="price">$179.95</div>
+								<div class="ctg-btn clearfix">
+									<span><a class="secondary-button" href="#">Shop Now</a></span>
+								</div>
+							</div>
+						</div>
+				   </div>
+			   </div>
+			   <div class="mob-6 tab-4 col-4">
+					<div class="shoe-wrapper__sub">
+						<div class="ctg-product">
+							<div class="offer-info">
+								<img src="/images/category/updated/runsig_icon_stacked_cushion.svg" alt="" width="100" height="100">
+							</div>
+							<div class="img img-shoes">
+								<img id="plp-img" src="/images/category/placeholder_shoe category.png" alt="">
+							</div>						
+							<div class="info">
+								<h3>PureFlow</h3>
+								<div class="price">$179.95</div>
+								<div class="ctg-btn clearfix">
+									<span><a class="secondary-button" href="#">Shop Now</a></span>
+								</div>
+							</div>
+						</div>
+				   </div>
+			   </div>
+			   <div class="mob-6 tab-4 col-4">
+					<div class="shoe-wrapper__sub">
+						<div class="ctg-product">
+							<div class="offer-info">
+								<img src="/images/category/updated/runsig_icon_stacked_cushion.svg" alt="" width="100" height="100">
+							</div>
+							<div class="img img-shoes">
+								<img id="plp-img" src="/images/category/placeholder_shoe category.png" alt="">
+							</div>						
+							<div class="info">
+								<h3>PureFlow</h3>
+								<div class="price">$179.95</div>
+								<div class="ctg-btn clearfix">
+									<span><a class="secondary-button" href="#">Shop Now</a></span>
+								</div>
+							</div>
+						</div>
+				   </div>
+			   </div>
+			   <div class="mob-6 tab-4 col-4">
+					<div class="shoe-wrapper__sub">
+						<div class="ctg-product">
+							<div class="offer-info">
+								<img src="/images/category/updated/runsig_icon_stacked_cushion.svg" alt="" width="100" height="100">
+							</div>
+							<div class="img img-shoes">
+								<img id="plp-img" src="/images/category/placeholder_shoe category.png" alt="">
+							</div>						
+							<div class="info">
+								<h3>PureFlow</h3>
+								<div class="price">$179.95</div>
+								<div class="ctg-btn clearfix">
+									<span><a class="secondary-button" href="#">Shop Now</a></span>
+								</div>
+							</div>
+						</div>
+				   </div>
+			   </div>
+		    </div>
 		</div>
-		<div class="col-3 contact-details--right">
-			<div class="info-wrapper">
-				<div class="icon">
-					<img src="images/brooks_aus_icons_email.svg" alt="">
+	<div class="row">
+			<div class="ctg-wrapper-container">
+				<div class="mob-12 tab-12 col-12 br-padding_bottm">
+					<div class="heading">
+					<h3 class="br-heading-detail">Specification</h3>
 				</div>
 				<div class="info">
-					<h3 class="br-heading">Email us</h3>
-				
-					<p><a href="http://brooks.syginteractive.com/contact-us"><u>Fill in our email support form</u></a> and we'll get back to you shortly.</p>
+				<div class="row">
+				<div class="col-3 mob-6">
+					<div class="info">
+						<h3>SHOE CATEGORY:</h3>
+						<h3>BODY BUILD:</h3>
+						<h3>DNA LAYOUT:</h3>
+					</div>
 				</div>
-			</div>
-			<div class="info-wrapper">
-				<div class="icon">
-					<img src="images/brooks_aus_icons_phone.svg" alt="">
+				<div class="col-3 mob-6">
+					<div class="info">
+						<h4>X-TRAINING</h4>
+						<h4>SMALL, MEDIUM, LARGE</h4>
+						<h4>FULL-LENGTH BLENDED BIOMOGO DNA LAYOUT</h4>
+					</div>
 				</div>
-				<div class="info">
-					<h3 class="br-heading">Call us</h3>
-					<p>Australia: 1300 735 099 <br/>New Zealand: 08 0061 3502 <br/>We're available to help you 
-						<br/>Mon - Fri 9am - 5pm AEST</p>
+				<div class="col-3 mob-6">
+					<div class="info">
+						<h3>ARCH:</h3>
+						<h3>PLATFORM:</h3>
+						<h3>CONSTRUCTION TYPE:</h3>
+						<h3>WEIGHT:</h3>
+					</div>
+				</div>
+				<div class="col-3 mob-6">
+					<div class="info">
+						<h4>MEDIUM, HIGH</h4>
+						<h4>UNIVERSAL PLATFORM</h4>
+						<h4>STROBEL</h4>
+						<h4>300G (WOMENS) / 360G (MENS)</h4>
+					</div>
+				</div>
+				</div>
+				</div>
 				</div>
 			</div>
 		</div>
 	</div>
 </section>
+</div>
+
 
 @endsection
