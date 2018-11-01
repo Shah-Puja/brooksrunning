5f69f44906f9444b7cbbb0e7931ca1f9ee872c81
Puja Shah <shahpuja1990@gmail.com>
2018-11-01 17:28:31 +0530

    Store Locator

--- resources/views/info/stores.blade.php
+++ resources/views/info/stores.blade.php
@@ -1,4 +1,6 @@
-<?xml version="1.0"?>
+<?php
+echo '<?xml version="1.0"?>';
+?>
 <markers>
 	@foreach ($stores as $store)
 	<marker id="{{ $store->id }}" name="{{ $store->name }}" address="{{ $store->address }}" lat="{{ $store->lat }}" lng="{{ $store->lng }}" distance="{{ $store->distance }}"/>
