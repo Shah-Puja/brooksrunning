acb32b2d4488871536632cf572c8a538a2918020
[sachintiwari809@gmail.com] <[sachintiwari809@gmail.com]>
2018-11-02 11:57:14 +0530

    sitemap-changes

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -4,38 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-<<<<<<< HEAD
-class InfoController extends Controller
-{
-  public function index($pg){ 
-   
-      return view( 'info.'.$pg);
-  }
-    public function help()
-	{
-		return view( 'info.help');
-=======
 class InfoController extends Controller {
 
     public function index($pg) {
 
         return view('info.' . $pg);
->>>>>>> af6b100e7d71ae470cb239303265c4f9ee8f8ecb
     }
 
     public function help() {
         return view('info.help');
     }
-<<<<<<< HEAD
+
     public function sitemap()
 	{
 		return view( 'info.sitemap');
     }
-=======
+
 
     public function store_locator() {
         return view('info.store_locator');
     }
 
->>>>>>> af6b100e7d71ae470cb239303265c4f9ee8f8ecb
+
 }
