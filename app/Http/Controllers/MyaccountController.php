9ff237a1095de229bd1221ad38a0c61a1187c37a
Puja Shah <shahpuja1990@gmail.com>
2018-11-05 18:14:48 +0530

    Account Personal details, Cart Promo changes

--- app/Http/Controllers/MyaccountController.php
+++ app/Http/Controllers/MyaccountController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class MyaccountController extends Controller
 {
@@ -17,13 +19,24 @@ class MyaccountController extends Controller
     }
     public function account_personal()
   	{
+        //User::where('email', $this->order->address->email)->update(['person_idx'=> $PersonID]);
 		  return view( 'customer.myaccount.account-personal');
-<<<<<<< HEAD
     }
     public function order_history()
     {
         return view( 'customer.myaccount.order-history');
-=======
->>>>>>> 78e98de72e4e6baa84decb93b53e8b4807354a4f
+    }
+
+    public function update_profile(Request $request){
+        /*echo "<pre>"; 
+        print_r($request->all());die;*/
+        User::where('id', auth()->id())->update(['first_name' => $request->first_name,'last_name' => $request->last_name, 'gender' => ucfirst($request->gender),
+        'dob' => $request->birth_month."-".$request->birth_date,'birth_date' => $request->birth_date,
+        'birth_month' => $request->birth_month, 'age_group' => $request->age_group, 'state' => $request->state, 
+        'postcode'=>$request->postcode, 'shoe_wear'=>$request->shoe_wear ? $request->shoe_wear : '', 'password'=> Hash::make($request->password),
+        'newsletter'=> @$request->newsletter ? 1 : 0
+        ]);
+        return redirect('account-personal');
+
     }
 }
