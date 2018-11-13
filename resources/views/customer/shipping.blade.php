91a13f9ac55b69fc0c64dd21c1a3355305bb1cf2
suraj dalvi <sdalvi431@gmail.com>
2018-11-13 12:48:58 +0530

    billing shipping error resolve

--- resources/views/customer/shipping.blade.php
+++ resources/views/customer/shipping.blade.php
@@ -284,7 +284,7 @@
                                         </div>
                                         <div class="radio-inline">
                                             <input type="radio" id="different-address" class="input-radio" name="flag_same_shipping" value="No">
-                                            <label for="different-address" onchange="valueChanged()">
+                                            <label for="different-address">
                                                 <div class="mark"><span></span></div>
                                                 <div class="text">Different Billing Address</div>
                                             </label>
@@ -357,13 +357,13 @@
                                             <?php
                                                 $error_b_state="";
                                                 $state = old('b_state');
+                                                echo $state;
                                                 if ($errors->has('b_state') ):
                                                     $error_b_state="<span class='error'>".$errors->first('b_state')."</span>";
                                                 endif;
                                             ?>
                                             <label for=""><sup>*</sup>State: {!! $error_b_state !!}</label>
                                             <select class="select-field" name="b_state" data-label-name="state">
-                                                <option value="" selected="selected">Select State</option>
                                                 <option value="" selected="selected">Select State</option>
                                                 <option value="ACT" {{ $state =='ACT' ? "selected='selected'": "" }}>ACT</option>
                                                 <option value="NSW" {{ $state =='NSW' ? "selected='selected'": "" }}>NSW</option>
