d56faa9ab89a721a3b64acb0d265e391d45bc0bb
Puja Shah <shahpuja1990@gmail.com>
2018-11-14 11:04:59 +0530

    Promo code in order

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -30,7 +30,7 @@ class Order extends Model
          /*echo "<pre>";
          print_R($cart);
          echo "</pre>";
-         die;*/
+         die;*/ 
         $order = self::updateOrCreate(
             [
                 'user_id' => $cart['user_id'],
@@ -54,6 +54,14 @@ class Order extends Model
 
         $order->orderItems()->delete();
         $promo_code = isset($cart->promo_code) ? $cart->promo_code : "";
+
+        if($promo_code!=""){
+            $order->update([
+                'promo_code' => ($promo_code) ? $promo_code : "",
+                'discount' => isset($cart->discount) ? $cart->discount : ""
+            ]);
+        }
+        
         
         $cart->cartItems->each(function($item) use ($order) {
             $order->orderItems()->create([
