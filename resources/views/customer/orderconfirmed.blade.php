98d7e24e70bcb2a72770335982dac0960f6f0928
Puja Shah <shahpuja1990@gmail.com>
2018-11-19 10:56:52 +0530

    order success

--- resources/views/customer/orderconfirmed.blade.php
+++ resources/views/customer/orderconfirmed.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <section class="wrapper cart-breadcrumb--header">
     @php
-     echo "<pre>";
-            print_r($order);
-         echo "</pre>";die;
+    // echo "<pre>";
+        //     print_r($order);
+        // echo "</pre>";
     @endphp
     <div class="row hidden-xs">
         <div class="col-9">
@@ -42,7 +42,7 @@
                 </div>
                 <div class="shoppingcart-wrapper">
                     <div class="shoppingcart-products">
-                    @php echo "<pre>";print_R($order); echo "<hr>";die; @endphp
+                    @php //echo "<pre>";print_R($order); echo "<hr>"; @endphp
                         @foreach($order->orderItems as $orderItem)
                         <div class="row">
                             <div class="col-3 tab-6">
@@ -95,7 +95,10 @@
                                     @endif
                                     <div class="row price">
                                         <div class="mob-5"><p>Item Total</p></div>
-                                        <div class="mob-7"><p class="right">&dollar;{{ number_format($orderItem->total, 2) }}</p></div>
+                                        @php  
+                                        $subtotal = $orderItem->total; 
+                                        @endphp
+                                        <div class="mob-7"><p class="right">&dollar;{{ number_format($subtotal, 2) }}</p></div>
                                     </div>
                                 </div>
                             </div>
