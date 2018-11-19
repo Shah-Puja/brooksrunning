767d73063c6f376c59e5058935ef98be57eda297
Puja Shah <shahpuja1990@gmail.com>
2018-11-19 11:10:01 +0530

    Order calc

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -67,6 +67,7 @@ class Order extends Model
         
         
         $cart->cartItems->each(function($item) use ($order) {
+            echo $item->discount_price."  ".$item->price_sale." item total--- ".$item->price_sale * $item->qty; 
             $order->orderItems()->create([
                      'variant_id' =>  $item->variant->id,
                      'style' =>  $item->variant->product->style,
@@ -79,6 +80,7 @@ class Order extends Model
                      'total' => ($item->discount_price != 0.00) ? ($item->discount_price * $item->qty) : ($item->price_sale * $item->qty)
              ]);
         });
+        die;
 
         $order->orderItems()->update([
             'promo_code' => ($promo_code) ? $promo_code : ""
