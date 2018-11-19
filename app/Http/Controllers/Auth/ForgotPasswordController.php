c6dc7e00ff582068b74eb7659295ca0207b27e8c
purvishah <purvi.cshah@gmail.com>
2018-11-19 16:52:40 +0530

    reset password

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
 
 class ForgotPasswordController extends Controller
 {
@@ -28,5 +30,25 @@ class ForgotPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+   
+    public function sendResetLinkEmail(Request $request)
+    {
+        $user=User::where('email',request('email'))
+        ->where('user_type','!=','User')->first();
+        if($user) return redirect('login')->withErrors(['email' => 'You are not registered with us.']);
+
+        $this->validateEmail($request);
+
+        // We will send the password reset link to this user. Once we have attempted
+        // to send the link, we will examine the response then see the message we
+        // need to show to the user. Finally, we'll send out a proper response.
+        $response = $this->broker()->sendResetLink(
+            $request->only('email')
+        );
+
+        return $response == Password::RESET_LINK_SENT
+                    ? $this->sendResetLinkResponse($request, $response)
+                    : $this->sendResetLinkFailedResponse($request, $response);
     }
 }
