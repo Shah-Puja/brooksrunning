9095f9f67d6c62c5d50aea6fabb45144db48d05b
trunalt <trunaltamore@gmail.com>
2018-12-20 16:22:33 +0530

    add .

--- app/Http/Controllers/MovetableController.php
+++ app/Http/Controllers/MovetableController.php
@@ -11,10 +11,13 @@ class MovetableController extends Controller
 
         $DB_from = "ftfjkzaeke"; // staging
         $DB_to = "gkkwquthrm"; // production
-        DB::statement("DROP TABLE $DB_to.newtable;");
-        
-        DB::statement("CREATE TABLE $DB_to.newtable LIKE $DB_from.p_products;");
 
-        DB::statement("INSERT INTO $DB_to.newtable SELECT * FROM $DB_from.p_products");
+        //$DB_from = "brookstest_2018"; // staging
+        //$DB_to = "chatbox"; // production
+        //DB::connection('mysql2')->statement("DROP TABLE IF EXISTS $DB_to.newtable;");
+        
+        DB::connection('mysql2')->statement("CREATE TABLE $DB_to.newtable LIKE $DB_from.p_products;");
+
+        DB::connection('mysql2')->statement("INSERT INTO $DB_to.newtable SELECT * FROM $DB_from.p_products");
     }
 }
