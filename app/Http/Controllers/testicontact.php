0ae25f7225522fcadeac8e0854689bfe9a5ec45c
Puja Shah <shahpuja1990@gmail.com>
2018-12-27 16:25:42 +0530

    Icontact

--- app/Http/Controllers/testicontact.php
+++ app/Http/Controllers/testicontact.php
@@ -4,17 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\SYG\Subscribers\iContactProApi;
-use App\SYG\Subscribers\SubscriberInterface;
+//use App\SYG\Subscribers\SubscriberInterface;
 
 class testicontact extends Controller {
-
+protected $client;
     public function __construct(iContactProApi $client) { 
         $this->client = $client;
     }
 
-    public function add($subscriber) {
-        $subscriber->email = 'puja_shah10@yahoo.co.in';
-        $response = $this->client->addContact($subscriber->email, null, null, null, null, null, null, null, null, null, null, null, null, null);
+    public function add() {
+        //$subscriber->email = 'puja_shah10@yahoo.co.in';
+        $response = $this->client->addContact('puja_shah10@yahoo.co.in', null, null, null, null, null, null, null, null, null, null, null, null, null);
         dump($response->contactId);
         $response = $this->client->subscribeContactToList($response->contactId, 1, 'normal');
         // dump($response);
