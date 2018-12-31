5d1c516ab3d18545dcd6ee4ce76719ae5a59e8a4
Puja Shah <shahpuja1990@gmail.com>
2018-12-31 15:30:17 +0530

    Icontact changeS

--- app/Http/Controllers/testicontact.php
+++ app/Http/Controllers/testicontact.php
@@ -2,19 +2,21 @@
 
 namespace App\Http\Controllers;
 
-
 use App\SYG\Subscribers\iContactProApi;
 use App\SYG\Subscribers\SubscriberInterface;
 
 class testicontact extends Controller {
-protected $client;
-    public function __construct(iContactProApi $client) { 
+
+    protected $client;
+
+    public function __construct(SubscriberInterface $client) {
+        //echo "dddddddd";die;
         //$client = \App\SYG\Subscribers\iContactProApi::getInstance();
         $this->client = $client;
     }
 
-    public function addiContact() {
-        //$subscriber->email = 'puja_shah10@yahoo.co.in';
+    public function add() {
+        $subscriber = new testicontact($this->client);
         $response = $this->client->addiContact('puja_shah11@yahoo.co.in', null, null, null, null, null, null, null, null, null, null, null, null, null);
         dump($response->contactId);
         $response = $this->client->subscribeContactToList($response->contactId, 1, 'normal');
--- app/SYG/Subscribers/iContactSubscriber.php
+++ app/SYG/Subscribers/iContactSubscriber.php
@@ -1,20 +1,26 @@
 <?php
+
 namespace App\SYG\Subscribers;
+
 use App\SYG\Subscribers\iContactProApi;
 use App\SYG\Subscribers\SubscriberInterface;
-class iContactSubscriber implements SubscriberInterface
-{
-	protected $client;
-	public function __construct(iContactProApi $client)
-	{
-		$this->client = $client;
-	}
-	public function addiContact($subscriber)
-	{
-		echo "eeeeeee";
-		$response = $this->client->addContact($subscriber->email, null, null, null, null, null, null, null, null, null, null, null, null, null);
-		dump($response->contactId);
-		$response = $this->client->subscribeContactToList($response->contactId, 1, 'normal');
-		// dump($response);
-	}
+
+class iContactSubscriber implements SubscriberInterface {
+
+    protected $client;
+
+    public function __construct(iContactProApi $client) {
+        $this->client = $client;
+    }
+
+    public function addiContact($subscriber) {
+
+        $response = $this->client->addContact('puja_shah13@yahoo.co.in', null, null, null, null, null, null, null, null, null, null, null, null, null);
+        echo "<pre>";
+        print_r($response);
+        die;
+        $response = $this->client->subscribeContactToList($response->contactId, 1, 'normal');
+        // dump($response);
+    }
+
 }
