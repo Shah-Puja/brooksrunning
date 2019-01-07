2420ab022eaa8046954b33ca41c5b285a414e6b9
Puja Shah <shahpuja1990@gmail.com>
2019-01-07 14:23:10 +0530

    Icontact Testing

---
+++ app/Http/Controllers/testicontact.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\SYG\Subscribers\iContactProApi;
+use App\SYG\Subscribers\SubscriberInterface;
+
+class testicontact extends Controller {
+
+    protected $client;
+
+    public function __construct(SubscriberInterface $client) {
+        //echo "dddddddd";die;
+        //$client = \App\SYG\Subscribers\iContactProApi::getInstance();
+        $this->client = $client;
+    }
+
+    public function add() {
+        $subscriber = new testicontact($this->client);
+        //here things need to do in following lines when integrated with system:
+        //1. to fetch user input details and pass email id
+        //2. add icontactid in table while registration and during profile update chk icontact id exists or not, if not then add new contact in the list
+        //3. When new competition is there, need to add new list and subscribe the users in that new list
+        $response = $this->client->addiContact('puja_shah199@yahoo.com', null, null, null, null, null, null, null, null, null, null, null, null, null);
+        echo "<pre>";
+        print_R($response->contactId);
+        $response = $this->client->subscribeContactToList($response->contactId, 1, 'normal');
+        echo "In TestiContact for testing Controller";
+        die;
+        // dump($response);
+    }
+
+}
---
+++ app/SYG/Subscribers/iContactSubscriber.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\SYG\Subscribers;
+
+use App\SYG\Subscribers\iContactProApi;
+use App\SYG\Subscribers\SubscriberInterface;
+
+class iContactSubscriber implements SubscriberInterface {
+
+    protected $client;
+
+    public function __construct(iContactProApi $client) {
+        $this->client = $client;
+    }
+
+    public function addiContact($subscriber) {
+        $response = $this->client->addContact('puja_shah198@yahoo.co.in', null, null, null, null, null, null, null, null, null, null, null, null, null);
+        echo "<pre>";
+        print_r($response);
+
+        $response = $this->client->subscribeContactToList($response->contactId, 1, 'normal');
+        echo "In iContactSubscriber";
+        echo "<pre>";
+        print_r($response);
+    }
+
+}
