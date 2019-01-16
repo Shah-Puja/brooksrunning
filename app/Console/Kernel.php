f975aff834e545dfcc99820a6b5ed2e3da396e79
Puja Shah <shahpuja1990@gmail.com>
2019-01-16 14:23:13 +0530

    iContact Scheduling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,11 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')
-        //          ->hourly();
+        //          ->hourly(); 
+        $schedule->command('demo:cron')
+                 ->everyMinute();
+
+        
     }
 
     /**
