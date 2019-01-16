ce165b5e2fa0bf5e8497062b92e5df88dc8953b1
trunalt <trunaltamore@gmail.com>
2019-01-16 13:39:07 +0530

    PLP back functionality and PDP AP21 sync

--- app/Http/Controllers/ProductColourController.php
+++ app/Http/Controllers/ProductColourController.php
@@ -108,7 +108,7 @@ class ProductColourController extends Controller
                     $color_idx = $curr_color->Id;
 				    $color_code = $curr_color->Code;									
                     $color_name = $curr_color->Name;
-                    $release_dt = Ap21_colour::where('clridx',$color_idx)->first();
+                    echo $release_dt = Ap21_colour::where('clridx',$color_idx)->first();
                     if ($release_dt == ""):
                         $release_dt_str = "0000-00-00";
                     else:
@@ -171,7 +171,7 @@ class ProductColourController extends Controller
 
                         //echo "sku_id " .$sku_id."<br>";
                         //Update p_variant
-                        Variant::where('id',$sku_id)->update($prod_data);
+                        ///Variant::where('id',$sku_id)->update($prod_data);
 
                     } // second foreach
                 } // first foreach
