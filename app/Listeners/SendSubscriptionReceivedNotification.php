e848640e67a75fdef55ff6c9e8c0b629dc7f53f7
Puja Shah <shahpuja1990@gmail.com>
2019-01-30 16:32:03 +0530

    Newsletter signup

--- app/Listeners/SendSubscriptionReceivedNotification.php
+++ app/Listeners/SendSubscriptionReceivedNotification.php
@@ -1,51 +1,58 @@
 <?php
+
 namespace App\Listeners;
 
 use App\Events\SubscriptionReceived;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use App\SYG\Subscribers\SubscriberInterface;
+use App\Models\Order_address;
 
-class SendSubscriptionReceivedNotification implements ShouldQueue
-{
+class SendSubscriptionReceivedNotification implements ShouldQueue {
+
     protected $subscriptionService;
+
     /**
      * Create the event listener.
      *
      * @return void
      */
-    public function __construct(SubscriberInterface $subscriptionService)
-    {
+    public function __construct(SubscriberInterface $subscriptionService) {
         $this->subscriptionService = $subscriptionService;
     }
+
     /**
      * Handle the event.
      *
      * @param  SubscriptionReceived  $event
      * @return void
      */
-    public function handle(SubscriptionReceived $event)
-    {
+    public function handle(SubscriptionReceived $event) {
+        /* echo "eeeeeeeeeeeeeeeeeeeee";echo "<pre>";print_r($event);die;
+          dd($event); */
 
-        if($event->user->source=='Order'){
-            echo "eeeeeeeeeeeeeeeeeeeee";die;
-        }
-        echo "eeeeeeeeeeeeeeeeeeeee";echo "<pre>";print_r($event);die;
-        dd($event);
         $subscriber = array(
             'email' => $event->user->email,
-            'name' => $event->user->first_name." ".$event->user->last_name,
-            'ad_tracking' => $event->user->source,            
+            'name' => $event->user->first_name . " " . $event->user->last_name,
+            'ad_tracking' => $event->user->source,
             'custom_fields' => array(
                 'Post Code' => $event->user->postcode,
                 'Gender' => $event->user->gender,
-                'Birth Day'=> $event->user->birth_date,
+                'Birth Day' => $event->user->birth_date,
                 'Birth Month' => $event->user->birth_month,
                 'Age' => $event->user->age_group,
                 'State' => $event->user->state
-                ),                        
-            );          
-        $this->subscriptionService->updateoradd_Subscriber($subscriber);
-    }
-}
+            ),
+        );
 
+        if ($event->user->source == 'Order') {
+            $user_detail = Order_address::where('email', '=', $event->user->email)->first();
+            if (isset($user_detail) && $user_detail->signme == 1) {
+                $this->subscriptionService->updateoradd_Subscriber($subscriber);
+            }
+        } else if ($event->user->source != 'Order') {
+            $this->subscriptionService->updateoradd_Subscriber($subscriber);
+        }
+    }
+
+}
