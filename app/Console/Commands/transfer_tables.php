37bfd6dfdf5f7aea05469328eb01182b46bbd046
purvishah <purvi.cshah@gmail.com>
2019-02-28 14:11:06 +0530

    a

--- app/Console/Commands/transfer_tables.php
+++ app/Console/Commands/transfer_tables.php
@@ -15,7 +15,7 @@ class transfer_tables extends Command
      *
      * @var string
      */
-    protected $signature = 'brooks:transfer-product-tables';
+    protected $signature = 'transfer-product-tables';
 
     /**
      * The console command description.
@@ -47,25 +47,27 @@ class transfer_tables extends Command
         $prod_tables="p_products p_variants p_images p_tags groups";        
         $sql_path="storage/data/products/".$curr_dt."_products.sql";
         $cmd="mysqldump -u".env("LIVE_DB_USERNAME")."  -p".env("LIVE_DB_PASSWORD")." ".env("LIVE_DB_DATABASE")." ".$prod_tables." > ".$sql_path ;        
-        $process = new Process($cmd);
+        $this->run_process($cmd);
+        /*$process = new Process($cmd);
         try {
             $process->mustRun();
             $this->info('MysqlDump is created successfully.');
         } catch (ProcessFailedException $exception) {
             $this->error('The MysqlDump process has been failed.');
             print_r($exception);
-        }
+        }*/
         
     
         $import_cmd="mysql -u".env("FUTURE_DB_USERNAME")."  -p".env("FUTURE_DB_PASSWORD")." ".env("FUTURE_DB_DATABASE")." < ".$sql_path;        
-        $import_process = new Process($import_cmd);
+        $this->run_process($import_cmd);
+        /*$import_process = new Process($import_cmd);
         try {
             $import_process->mustRun();
             $this->info('Mysql import is done successfully.');
         } catch (ProcessFailedException $exception) {
             $this->error('The Mysql import has failed.');
             print_r($exception);
-        }
+        }*/
         
         DB::connection('future')->table("p_variants")->update(['visible' => 'No','reason_no'=>'Initial Setup']);
         DB::connection('future')->table("p_variants")->where('release_date','>',Date('Y-m-d'))->update(['visible' => 'Yes','reason_no'=>'']);
@@ -77,5 +79,17 @@ class transfer_tables extends Command
                     $message->Subject('Laravel mail');
                  });
         
+    }
+
+    public function run_process($cmd)
+    {   
+        $process = new Process($cmd);
+        try {
+            $process->mustRun();
+            $this->info('Process got executed successfully.');
+        } catch (ProcessFailedException $exception) {
+            $this->error('Process failed.');
+            print_r($exception);
+        }
     }
 }
