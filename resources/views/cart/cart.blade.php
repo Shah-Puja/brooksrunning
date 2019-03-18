2a9d0da3f82b4f717998a615ada7bb79bdf2b4e1
[sachintiwari809@gmail.com] <[sachintiwari809@gmail.com]>
2019-03-18 10:11:45 +0530

    sundara-20190318

--- resources/views/cart/cart.blade.php
+++ resources/views/cart/cart.blade.php
@@ -23,9 +23,11 @@
             <div class="cart-left--container">
                 <div class="heading">
                     <h3 class="br-heading">Shopping Cart</h3>
-                    <span class="offer">Promotion Code has expired so we could not proceed to Payment. Please check your Cart Details. </span>
-                
-                    <span class="offer"><br/>Free shipping on orders over $50 Australia wide.</span>
+                    <!-- @if(session('promo_expire') == "Promo Expired") 
+                            <span class="offer">Promotion Code has expired so we could not proceed to Payment. Please check your Cart Details. </span>
+                    @endif  -->
+                    <br/>
+                    <span class="offer">Free shipping on orders over $50 Australia wide.</span>
                 </div>
                 @if(!empty($cart))
                 <button class="pdp-button pdp-proceed-mob proceed-to-purchase visible-mob" onclick="window.location.href = '/shipping'">Proceed to Purchase</button> 
