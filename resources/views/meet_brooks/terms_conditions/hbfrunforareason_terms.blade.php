146f4b5d1ca721c07ef6716059806c45287366c7
[sachintiwari809@gmail.com] <[sachintiwari809@gmail.com]>
2019-04-11 12:15:54 +0530

    Comp update

--- resources/views/meet_brooks/terms_conditions/hbfrunforareason_terms.blade.php
+++ resources/views/meet_brooks/terms_conditions/hbfrunforareason_terms.blade.php
@@ -3,33 +3,20 @@
 <ol>
   	<li>Information on &quot;How to Enter&quot; and prizes form part of these Conditions of Entry.</li>
   	<li>Entry is open to Australian residents, except employees of Texas Peak Pty Ltd or Bocara Limited and their immediate families.</li>
-  	<li>The promotional period commences at 12:00pm (AEST) on Monday 15th April 2019 and concludes at 12:00pm (AEST) on Sunday 19th May 2019 (‘Promotional Period’). The winner will be selected on Monday 20th May at 12:00pm AEST on the premises of Texas Peak, 30 Tullamarine Park Rd, Tullamarine, Vic, <i>Australia</i>. The winner will be notified by email.</li>
+  	<li>The promotional period commences at 12:00pm (AEST) on Monday 15th April 2019 and concludes at 12:00pm (AEST) on Sunday 19th May 2019 ('Promotional Period'). The winner will be selected on Monday 20th May at 12:00pm AEST on the premises of Texas Peak, 30 Tullamarine Park Rd, Tullamarine, Vic, <i>Australia</i>. The winner will be notified by email.</li>
   	<li>To enter participants must complete a competition entry form at <a href="https://www.brooksrunning.com.au/meet_brooks/competition/hbfrunforareasoncomp">https://www.brooksrunning.com.au/meet_brooks/competition/hbfrunforareasoncomp</a> during the dates specified above. It is the participant's responsibility to ensure all details are full and correct before submitting their entry form.</li> 
   	<li>Entries not completed in accordance with the Conditions of Entry or received after close of entry will not be considered. Incomplete or incorrect entries will be disregarded. The Promoter reserves the right to verify the validity of entries and reserves the right to disqualify any entrant for tampering with the entry process. The judge&apos;s decision in relation to any aspect of the Promotion is final and binding on every person who enters.  </li>
   	<li>The winner of the Promotion will be chosen at random. The prizes must be claimed before 5pm AEST on 10th June 2019 otherwise a redraw will occur. If in the event the prize is not claimed a redraw will occur on the 11th June 2019 at 12:00pm AEST at the premises of Texas Peak. The winner will be notified by email.</li>
-  	<li>Each prize is as follows:
-      <p>MENS (RRP: $1,010)</p>
-		<ul>
-        		<li>	FitBit Ionic (RRP:$399.95)</li>    
-				<li>	Transcend (RRP: $250)</li>    
-				<li>	LSD Jacket (RRP: $119.95)</li>    
-				<li>	Sherpa 5inch Short (RRP: $90)</li>    
-				<li>	Distance Short Sleeve (RRP: $60)</li>    
-				<li>	RH Sherpa Hat (RRP: $50)</li>    
-				<li>	Running socks (RRP: $20)??</li>            
-		</ul>
-    <p>WOMENS (RRP: $1,020)</p>
-    <ul>
-          	<li>FitBit Ionic (RRP:$399.95)</li>
-			<li>	Transcend (RRP: $250)</li>
-			<li>	LSD Jacket (RRP: $119.95)</li>
-			<li>	Greenlight Capri tights (RRP: $100) </li>
-			<li>	Distance Short sleeve (RRP: $60)</li>
-			<li>	RH Sherpa Hat (RRP: $50)</li>
-			<li>	Running socks (RRP: $20)?? </li>         
-    </ul>
-    <p>&nbsp;</p>
-  	</li> 
+  	<li>7.	The prize is as follows:
+    		<ul>
+            	<li>FitBit Ionic (RRP:$399.95)</li>
+				<li>Brooks Footwear (Valued up to RRP: $259.95)</li>
+				<li>LSD Jacket (RRP: $119.95)</li>
+				<li>Sherpa 5inch Short (RRP: $90) / Greenlight Capri tights (RRP: $100)</li>
+				<li>Stealth Short Sleeve (RRP: $79.95) / Pick-Up Tank (RRP: $79.95)</li>
+				<li>RH Sherpa Hat (RRP: $49.95)</li>         
+    		</ul>
+    </li>
   	<li>The Promoter reserves the right to request winners to provide proof of their identity and proof of Australian residency in order to claim the prize. Proof of identification and residency is at the judgment of the promoter.</li>
     <li>Entrants must be over 18 years of age, or have permission from their Parent or Guardian.</li>
     <li>All prizes must be taken as offered. The prize, or any unused portion of the prize, is not transferable or exchangeable and cannot be taken as cash. The Promoter accepts no responsibility for any variation in the prize value.</li>
