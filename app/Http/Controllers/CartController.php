b2c61bda291ec7e4b935192c371c3c7fa09b8395
Puja Shah <shahpuja1990@gmail.com>
2019-04-24 14:47:26 +0530

    ap21 down chk

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -83,22 +83,24 @@ class CartController extends Controller {
                 /* echo "Cart items";echo "<pre>";
                   print_r($cart_arr['cart_items']);
                   die; */
-                  $total = 0;
-                foreach ($cart_arr['cart_items'] as $item) {
-                    $price_sale = isset($item['price_sale']) ? $item['price_sale'] : 0;
-                    if (isset($price_sale) && $price_sale != 0) {
-                        $sku = isset($item['variant_id']) ? $item['variant_id'] : $item['skuidx'];
-                        $qty = $item['qty'];
-                        $total += $item['price_sale'] * $item['qty'];
-                        Cart_item::where('variant_id', $sku)->where('cart_id', session('cart_id'))->update(['discount_price' => $price_sale, 'discount_detail' => 0, 'price_sale' => $price_sale]);
+                $total = 0;
+                if (!empty($cart_arr)) {
+                    foreach ($cart_arr['cart_items'] as $item) {
+                        $price_sale = isset($item['price_sale']) ? $item['price_sale'] : 0;
+                        if (isset($price_sale) && $price_sale != 0) {
+                            $sku = isset($item['variant_id']) ? $item['variant_id'] : $item['skuidx'];
+                            $qty = $item['qty'];
+                            $total += $item['price_sale'] * $item['qty'];
+                            Cart_item::where('variant_id', $sku)->where('cart_id', session('cart_id'))->update(['discount_price' => $price_sale, 'discount_detail' => 0, 'price_sale' => $price_sale]);
+                        }
                     }
                 }
-                
+
                 $cart_total = $total;
                 $total_discount = 0;
                 $freight_charges = $cart_arr['freight_cost'];
-                      
-                Cart::where('id', session('cart_id'))->update(['total' => $cart_total, 'freight_cost' => $freight_charges, 'discount' => $total_discount,'grand_total' => $freight_charges + $cart_total]);
+
+                Cart::where('id', session('cart_id'))->update(['total' => $cart_total, 'freight_cost' => $freight_charges, 'discount' => $total_discount, 'grand_total' => $freight_charges + $cart_total]);
             }
             $cart = Cart::where('id', session('cart_id'))->with('cartItems.variant.product:id,gender,stylename,color_name,cart_blurb')->first();
         }
