27c2160546397ef2d2eaddb9a9ff95b43d1e0d85
trunalt <trunaltamore@gmail.com>
2019-05-13 12:57:35 +0530

    new change

--- app/Http/Controllers/MedibankController.php
+++ app/Http/Controllers/MedibankController.php
@@ -37,12 +37,15 @@ class MedibankController extends Controller
             if(!$err) {
                 $response = json_decode($response,true);
                 if(!empty($response)){
+                    
                    $access_token = $response['access_token'];
 
                     $curl = curl_init();
                     $PolicyNumber = Input::get('medibank_id');
                     $email = Input::get('email');
                     $corporate_id = env('MEDIBANK_CORPORATEID');
+                    $RequestID = (new static)->generateUUID();
+
                     curl_setopt_array($curl, array(
                       CURLOPT_URL => env('MEDIBANK_VERIFY_MEMBER_URL'),
                       CURLOPT_RETURNTRANSFER => true,
@@ -57,6 +60,7 @@ class MedibankController extends Controller
                         "Authorization: $access_token",
                         "Content-Type: application/json",
                         "X-CSRF-TOKEN:". csrf_token(),
+                        "RequestID:".$RequestID,
                         "cache-control: no-cache"
                       ),
                     ));
@@ -93,5 +97,27 @@ class MedibankController extends Controller
             return 'false';
         }
        
+    }
+
+    public static function generateUUID() {
+            return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            // 32 bits for "time_low"
+            mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+
+            // 16 bits for "time_mid"
+            mt_rand( 0, 0xffff ),
+
+            // 16 bits for "time_hi_and_version",
+            // four most significant bits holds version number 4
+            mt_rand( 0, 0x0fff ) | 0x4000,
+
+            // 16 bits, 8 bits for "clk_seq_hi_res",
+            // 8 bits for "clk_seq_low",
+            // two most significant bits holds zero and one for variant DCE1.1
+            mt_rand( 0, 0x3fff ) | 0x8000,
+
+            // 48 bits for "node"
+            mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+        );
     }
 }
