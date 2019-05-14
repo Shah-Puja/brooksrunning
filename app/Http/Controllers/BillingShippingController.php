5e0f5c3b3c864736a29a3144c990dc7c4ef05b53
Puja Shah <puja.shah@orionsolution.com>
2019-05-14 14:16:39 +0530

    code commented

--- app/Http/Controllers/BillingShippingController.php
+++ app/Http/Controllers/BillingShippingController.php
@@ -45,14 +45,14 @@ class BillingShippingController extends Controller
             $usersLastOrder = Order::where('user_id', auth()->id())
                                 ->orderBy('updated_at', 'desc')
                                 ->first();
-            if(isset($usersLastOrder->address->s_state) && $usersLastOrder->address->s_state=='New Zealand'){
+            /*if(isset($usersLastOrder->address->s_state) && $usersLastOrder->address->s_state=='New Zealand'){
                 $delivery_option = "new_zealand";
                 $freight_charges = config('site.SHIPPING_NZ_PRICE');
                 $grand_total = $this->cart->total + $freight_charges;
                 Cart::where('id', session('cart_id'))->update(['delivery_type' => $delivery_option, 'freight_cost' => $freight_charges, 'grand_total' => $grand_total]);
                 $this->cart = Cart::where('id', session('cart_id'))->with('cartItems.variant.product:id,style,stylename,color_name')->first();
                 Order::where('user_id', auth()->id())->update(['delivery_type' => $delivery_option, 'freight_cost' => $freight_charges, 'grand_total' => $grand_total]);
-            }
+            }*/
             $orderAddress = $usersLastOrder ? $usersLastOrder->address : null; 
         }
         if (! @$orderAddress) {
