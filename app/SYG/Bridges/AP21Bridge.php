3d06232d1e702c400fda1a5249207c33eaf037a6
Puja Shah <puja.shah@orionsolution.com>
2019-05-30 10:04:28 +0530

    detect ap21

--- app/SYG/Bridges/AP21Bridge.php
+++ app/SYG/Bridges/AP21Bridge.php
@@ -23,7 +23,7 @@ class AP21Bridge implements BridgeInterface {
 			}
 		}
 		catch (RequestException $e) { 
-			throw $e;
+			echo "eeeeeeeeeeeeeeeeeeeeeeeeeeeee";throw $e;
 		}
 		catch (\Exception $exception) {
 			throw $exception;
