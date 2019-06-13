e863f538f2f6fe2cccc8a878f8067c86514fe8a7
Puja Shah <puja.shah@orionsolution.com>
2019-06-13 11:07:09 +0530

    birthday-month

--- app/Http/Controllers/testicontact.php
+++ app/Http/Controllers/testicontact.php
@@ -107,8 +107,8 @@ class testicontact extends Controller {
            // $userid = $user->id;
             echo "<br>" . $email;
             $dob = explode("-", $user->dob);
-            $month = (isset($user->dob[0]) && $user->dob[0]!="") ? $user->dob[0] : "";
-            $day = (isset($user->dob[1]) && $user->dob[1]!="") ? $user->dob[1] : "";
+            $month = (isset($dob[0]) && $dob[0]!="") ? $dob[0] : "";
+            $day = (isset($dob[1]) && $dob[1]!="") ? $dob[1] : "";
 
             $person_arr = array('name' => $user->fname . " " . $user->lname, 'email' => trim($user->email),
             'contest_code' => $user->comp_name,'ad_tracking' => $user->source, 'gender' => $user->gender, 
