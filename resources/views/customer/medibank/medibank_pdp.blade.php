e86eae957c676a81048f27e496b500bf624602dd
[sachintiwari809@gmail.com] <[sachintiwari809@gmail.com]>
2019-06-24 13:49:36 +0530

    pdp text update

--- resources/views/customer/medibank/medibank_pdp.blade.php
+++ resources/views/customer/medibank/medibank_pdp.blade.php
@@ -5,7 +5,7 @@
         <h3>For every $1 on spent on sale Brooks items, receive xx Live Better Points.</h3>
     @else
         <!-- <h3>For every $1 spent at Brooks on full price items, receive xx Live Better points</h3> -->
-        <h3>Earn 15 Live Better Points for every $1 spent on full price Brooks Runners and 10 for every $1 spent on discounted Brooks Runners purchased online at <a href="http://livebetter.brooks.com.au/" style="text-decoration:underline; color:#fff;">livebetter.brooksrunning.com.au</a>, with Medibank Live Better. Available to Medibank members with hospital or extras cover.*</h3>
+        <p>Earn 15 Live Better Points for every $1 spent on full price Brooks Runners and 10 for every $1 spent on discounted Brooks Runners purchased online at <a href="http://livebetter.brooks.com.au/" style="text-decoration:underline; color:#fff;">livebetter.brooksrunning.com.au</a>, with Medibank Live Better. Available to Medibank members with hospital or extras cover.*</p>
         <a href="#disclaimer" class="disclaimer-link">For full terms and conditions, please see below.</a>
     @endif
 </div>
