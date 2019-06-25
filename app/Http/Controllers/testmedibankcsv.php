0b38305aa8cceb8453043191bb430aa6ef70e30a
Puja Shah <puja.shah@orionsolution.com>
2019-06-25 14:11:55 +0530

    csv medibank

--- app/Http/Controllers/testmedibankcsv.php
+++ app/Http/Controllers/testmedibankcsv.php
@@ -1,49 +1,37 @@
 <?php
 
 namespace App\Http\Controllers;
-use Symfony\Component\HttpFoundation\Response;
+
 use App\Models\Order;
+use DB;
 
 class testmedibankcsv extends Controller {
 
     protected $client;
 
     public function __construct() {
+        
     }
 
-    public function export_medibank_order_csv() { 
-        $headers = array(
-            "Content-type" => "text/csv",
-            "Content-Disposition" => "attachment; filename=file.csv",
-            "Pragma" => "no-cache",
-            "Cache-Control" => "must-revalidate, post-check=0, pre-check=0",
-            "Expires" => "0"
+    public function export_medibank_order_csv() {
+        /*$columns = array('TransactionID', 'OrderReferenceID', 'TransactionTypeCode', 'RefundCorrelationID', 'CorporateID', 'PolicyNumber', 'GivenName', 'FamilyName', 'BirthDate', 'EmailURI', 'TransactionDateTime', 'TransactionLocation', 'EligibleTransactionTotal', 'CurrencyCode', 'TransactionTier');
+        $filename = date('dmYHis') . '.csv';
+        $out = fopen('../testcsv/' . $filename, 'w');
+        fputcsv($out, $columns);*/
+        $order_records = DB::table('orders')
+        ->join('order_addresses', 'orders.id', '=', 'order_addresses.order_id')
+        ->join('order_items', 'orders.id', '=', 'order_items.order_id')
+        ->where('orders.order_type', 'like', '%medibank%')->whereNull('orders.medibank_csv')
+        ->where('orders.transaction_status', 'Succeeded')
+        ->select('orders.*','order_addresses.email','order_addresses.s_fname','order_addresses.s_lname','order_items.price_sale','order_items.price')->first();
+        echo "<pre>";print_r($order_records);die;
+        /*fputcsv($out, array($order_record->id . "-" . $order_record->transaction_id, $order_record->order_no));
+        fclose($out);
+        //update medibank_csv field in order table
+        $orderDataUpdate = array(
+            'medibank_csv' => $filename
         );
-        $order_records_arr = array();
-        $order_records =Order::where('order_type', 'like' , '%medibank%')->where('transaction_status', 'Succeeded')->get();
-        foreach($order_records as $order_record){
-            $order_record_arr['transaction_id'] = $order_record->id."-".$order_record->transaction_id;
-            $order_record_arr['ref_id'] = $order_record->order_no;
-            $order_records_arr[] = $order_record_arr;
-        }
-         
-        $columns = array('TransactionID', 'OrderReferenceID');
-    
-        $callback = function() use ($order_records_arr, $columns)
-        {
-            $file = fopen('php://output', 'w');
-            fputcsv($file, $columns);
-    
-            foreach($order_records_arr as $order_record_arr) {
-                fputcsv($file, array($order_record_arr->transaction_id, $order_record_arr->ref_id));
-            }
-            fclose($file);
-        };
-        $pathToFile = '/testcsv/';
-        return response()->download($callback,['Content-Description' =>  'File Transfer','Content-Type' => 'application/octet-stream','Content-Disposition' => 'attachment; filename=import.csv']);
-
-
-        //return response()->streamDownload($callback, 'prefix-' . date('d-m-Y-H:i:s').'.csv', $headers);
+        Order::where('id', $order_record->id)->update($orderDataUpdate);*/
     }
 
 }
