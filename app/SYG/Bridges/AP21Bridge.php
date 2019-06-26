9d2db1049f0c3c59caeebea2aefcdc65617e1b4a
Puja Shah <puja.shah@orionsolution.com>
2019-06-26 16:35:34 +0530

    valid voucher chk

--- app/SYG/Bridges/AP21Bridge.php
+++ app/SYG/Bridges/AP21Bridge.php
@@ -1,78 +1,67 @@
 <?php
 
 namespace App\SYG\Bridges;
+
 use GuzzleHttp\Exception\RequestException;
 
 class AP21Bridge implements BridgeInterface {
 
-	protected $apiClient;
-	public function __construct($apiClient)
-	{
-		$this->apiClient = $apiClient;
-	}
+    protected $apiClient;
 
-	public function processCart($data)
-	{
-		//return $this->apiClient->put('Carts/1234?countryCode=AUFIT', ['body' => $data])->getBody();
-		try{
-			$response = $this->apiClient->put('Carts/1234?countryCode=AUFITs', ['body' => $data,'http_errors' => false]);
-			//echo $response->getStatusCode();
-			if($response->getStatusCode()==200){ 
-				return $response->getBody();
-			} 
-		}
-		catch (RequestException $e) { 
-			throw $e;
-		}
-		catch (\Exception $exception) {
-			throw $exception;
-		}
-	}
+    public function __construct($apiClient) {
+        $this->apiClient = $apiClient;
+    }
 
-	public function allProducts()
-	{
-		return $this->apiClient->get('Products?countryCode=AUFIT')->getBody();
-	}
+    public function processCart($data) {
+        //return $this->apiClient->put('Carts/1234?countryCode=AUFIT', ['body' => $data])->getBody();
+        try {
+            $response = $this->apiClient->put('Carts/1234?countryCode=AUFITs', ['body' => $data, 'http_errors' => false]);
+            //echo $response->getStatusCode();
+            if ($response->getStatusCode() == 200) {
+                return $response->getBody();
+            }
+        } catch (RequestException $e) {
+            throw $e;
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
+    }
 
-	public function getProduct($productCode)
-	{
-		return $this->apiClient->get('Products/' . $productCode . '?countryCode=AUFIT')->getBody();
-	}
-	
-	public function getPersonid($email)
-	{
-		return $this->apiClient->get('Persons/?countryCode=AUFIT&email=' . $email, ['http_errors' => false]);
-	}
+    public function allProducts() {
+        return $this->apiClient->get('Products?countryCode=AUFIT')->getBody();
+    }
 
-	public function processPerson($data)
-	{
-		return $this->apiClient->post('Persons/?countryCode=AUFIT', ['body' => $data , 'http_errors' => false]);
-	}
+    public function getProduct($productCode) {
+        return $this->apiClient->get('Products/' . $productCode . '?countryCode=AUFIT')->getBody();
+    }
 
-	public function processOrder($PersonId,$data)
-	{
-		return $this->apiClient->post('Persons/'.$PersonId.'/Orders/?countryCode=AUFIT', ['body' => $data , 'http_errors' => false]);
-	}
-	public function vouchervalid($gift,$pin,$amount)
-	{
+    public function getPersonid($email) {
+        return $this->apiClient->get('Persons/?countryCode=AUFIT&email=' . $email, ['http_errors' => false]);
+    }
+
+    public function processPerson($data) {
+        return $this->apiClient->post('Persons/?countryCode=AUFIT', ['body' => $data, 'http_errors' => false]);
+    }
+
+    public function processOrder($PersonId, $data) {
+        return $this->apiClient->post('Persons/' . $PersonId . '/Orders/?countryCode=AUFIT', ['body' => $data, 'http_errors' => false]);
+    }
+
+    public function vouchervalid($gift, $pin, $amount) {
         //return $this->apiClient->get('Voucher/GVValid/'.$gift.'?pin='.$pin.'&amount='.$amount.'&countryCode=AUFIT', ['http_errors' => false]);
-		try{
-			$response=$this->apiClient->get('Voucher/GVValid/'.$gift.'?pin='.$pin.'&amount='.$amount.'&countryCode=AUFIT', ['http_errors' => false]);
-			if($response->getStatusCode()==200){ 
-				return $this->apiClient->get('Voucher/GVValid/'.$gift.'?pin='.$pin.'&amount='.$amount.'&countryCode=AUFIT', ['http_errors' => false]);
-			} 
-		}
-		catch (\Exception $exception) {
-			throw $exception;
-		}        
-		//return $this->apiClient->get('Voucher/GVValid/'.$gift.'?pin='.$pin.'&amount='.$amount.'&countryCode=AUFIT')->getBody();
-        //return $this->apiClient->get('Voucher/GVValid/'.$gift.'?pin='.$pin.'&amount='.$amount.'&countryCode=AUFIT', ['http_errors' => false]);
-	}	
+        try {
+            $response = $this->apiClient->get('Voucher/GVValid/' . $gift . '?pin=' . $pin . '&amount=' . $amount . '&countryCode=AUFIT', ['http_errors' => false]);
+			echo "eeeeeeeeeee".$response->getStatusCode();
+			if ($response->getStatusCode() == 200) {
+                return $this->apiClient->get('Voucher/GVValid/' . $gift . '?pin=' . $pin . '&amount=' . $amount . '&countryCode=AUFIT', ['http_errors' => false]);
+            }
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
+    }
 
-	public function sync_ap21_sku($prod_styleidx)
-	{
-		return $this->apiClient->get('Products/'.$prod_styleidx.'?countryCode=AUFIT', ['http_errors' => false]);
-	}
+    public function sync_ap21_sku($prod_styleidx) {
+        return $this->apiClient->get('Products/' . $prod_styleidx . '?countryCode=AUFIT', ['http_errors' => false]);
+    }
 
 }
-
