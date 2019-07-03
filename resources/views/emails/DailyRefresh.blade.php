66705f14c1fbccf1c38386555fec64d03e6e4074
purvishah <purvi.cshah@gmail.com>
2019-07-03 16:55:52 +0530

    t4

--- resources/views/emails/DailyRefresh.blade.php
+++ resources/views/emails/DailyRefresh.blade.php
@@ -1,1 +1,9 @@
-# records in P_Products = {{$products}}
+# records in Products = {{$products}}
+# records in Variants = {{$variants}}
+# records in Tags = {{$tags}}
+# records in Images = {{$images}}
+# records in Groups = {{$groups}}
+# records in Ap21_notes = {{$ap21_notes}}
+# records in Ap21_notes_distinct = {{$ap21_notes_distinct}} (Distinct Style an Color IDX)
+# records in Ap21_processed_idx = {{$ap21_processed_idx}} (Should be ~= ap21_processed_idx )
+# records in Ap21_Unprocessed_idx = {{$ap21_unprocessed_idx}} (Should be 0)
