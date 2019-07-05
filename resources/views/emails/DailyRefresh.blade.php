dc84c5ba48efc5bc77f59ee1a43e34a1628b3de9
purvishah <purvi.cshah@gmail.com>
2019-07-05 14:45:49 +0530

    1

--- resources/views/emails/DailyRefresh.blade.php
+++ resources/views/emails/DailyRefresh.blade.php
@@ -1,4 +1,5 @@
 <h2>Live Database</h2>
+Does New tables Exists? = {{$new_table_exist}} *Expected Answer = No (If Yes, refresh incomplete) <br>
 # Visible Skus = {{$visible_sku}} <br>
 # Not Visible Skus = {{$not_visible_sku}} <br>
 # records in Products = {{$products}} <br>
