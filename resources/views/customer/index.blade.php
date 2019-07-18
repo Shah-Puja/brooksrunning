08e51608dc82f0aa87df6f4f4eaa00bdbe2270ce
[sachintiwari809@gmail.com] <[sachintiwari809@gmail.com]>
2019-07-18 10:34:20 +0530

    homepage images update

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -38,14 +38,13 @@
 		<div class="tab-4">
 			<div class="overgray-wrapper">
 				<div class="img-wrapper"> 
-					<img src="/images/home/hp_sm_energize.jpg">
+					<img src="/images/home/20190715/hp_sm_revel.jpg">
 			        <h3>&nbsp;<br/>&nbsp;</h3>
 				</div>
 				<div class="info-wrapper">
-					<p>Create a run that feels easier. Responsive and springy, these shoes add a lift to every stride. Choose from neutral or support shoes for a run customised to you.</p>
-					<a href="/collections/energize-running-shoes">Shop the Energize collection <img id="br-home" src="/images/home/link-arrow--icon.png" alt=""></a>
-					<!-- <span class="right-link"><a href="/brooks-running-shoes-launch-6-mens/110297_071.html">Shop men's <img id="br-home" src="/images/home/link-arrow--icon.png" alt="" ></a></span> -->
-				
+					<p>The new Revel 3 features a lower profile with a street-ready upper for days that have you running errands and workouts.</p>
+					<a href="/brooks-running-shoes-revel-3-mens/110314_088.html">Shop men's <img id="br-home" src="/images/home/link-arrow--icon.png" alt="" ></a>
+					<span class="right-link"><a href="/brooks-running-shoes-revel-3-womens/120302_012.html">Shop women's <img id="br-home" src="/images/home/link-arrow--icon.png" alt=""></a></span>				
 				</div>
 			</div>
 		</div>
