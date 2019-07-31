3507cf356d7a85ec9e95bd619e65ea077f0bf521
trunalt <trunaltamore@gmail.com>
2019-07-31 15:34:18 +0530

    new changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -72,7 +72,7 @@ class User extends Authenticatable
             $data['gender'] = "F";
         }
         $data['process'] = $process;    
-        $person_xml = response()->view('xml.person_xml',compact('data'))->header('Content-Type', 'text/xml');
+        $person_xml = response()->view('xml.person_xml',compact('data'));
         echo $person_xml;
         $response = $bridge->processPerson(simplexml_load_string($person_xml));
         print_r($response);
