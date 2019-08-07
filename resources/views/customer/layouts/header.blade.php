9bc1308136766b706d5d7908a823775718063090
[sachintiwari809@gmail.com] <[sachintiwari809@gmail.com]>
2019-08-07 16:49:15 +0530

    temp

--- resources/views/customer/layouts/header.blade.php
+++ resources/views/customer/layouts/header.blade.php
@@ -46,10 +46,7 @@
         <!-- common file -->
         <link rel="stylesheet" href="/css/common.css?v={{ Cache::get('css_version_number') }}">
         <script src="/js/jquery-2.2.4.min.js"></script>
-        <div class="br-afterpaysale-header">
-            <p class="br-afteryay-sale"><span class="br-afteryay-sale">AFTERYAY SALE! </span><br class="hidden-col hidden-tab visible-mob"/> 20% OFF full price styles. Use code YAY20.</p>
-            <p class="br-afteryay-sale-offer-valid">*Offer valid Wed 14 & Thurs 15 August. Excludes sale items.</p>
-        </div>
+       
         @yield('head')
         @include('digitalmarketingsnippets.gtm')
     </head>
