931f7c83a7f1b70bb34ffa6ff6d6dcb1346c5c04
[sachintiwari809@gmail.com] <[sachintiwari809@gmail.com]>
2019-08-09 11:28:04 +0530

    afterpay sale banner updated

---
+++ resources/views/customer/header_banner.blade.php
@@ -0,0 +1,6 @@
+@if(isset($header_banner->active) && $header_banner->active=='Y')
+<div class="br-afterpaysale-header">
+    <p class="br-afteryay-sale"><span class="br-afteryay-sale">AFTERYAY SALE! </span><br class="hidden-col hidden-tab visible-mob"/> 20% OFF full price styles. Use code YAY20.</p>
+    <p class="br-afteryay-sale-offer-valid">*Offer valid Wed 14 & Thurs 15 August. Excludes sale items.</p>
+</div>
+@endif
