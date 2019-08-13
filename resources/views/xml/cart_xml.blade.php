1c37f13364e1ac5966afd06ed42929914f562be8
trunalt <trunaltamore@gmail.com>
2019-08-13 15:40:23 +0530

    new changes

--- resources/views/xml/cart_xml.blade.php
+++ resources/views/xml/cart_xml.blade.php
@@ -9,11 +9,6 @@
 				<Quantity>{{$item->qty}}</Quantity>
 			</CartDetail>
         @endforeach
-		@if(isset($xml_promo_st) && $xml_promo_st!='')
-			<CartDetail>		
-				<SkuId>{{$xml_promo_st->skuid}}</SkuId>
-				<Quantity>{{$xml_promo_st->qty}}</Quantity>
-			</CartDetail>
-		@endif
+		
     </CartDetails>
 </Cart>
