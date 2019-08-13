60552951055d2775bd53403400017c1495cc0baf
trunalt <trunaltamore@gmail.com>
2019-08-13 15:01:18 +0530

    new changes

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -96,7 +96,7 @@ class Cart extends Model {
     public function cart_api($bridgeObject) {   /// new function
         if($this->cartItems->count() > 0){
             $xml_promo_st = $this->get_promo_xml();
-            print_r($cart_xml);
+            print_r($xml_promo_st);
             exit;
             $cart_xml = view('xml.cart_call')->with(['cart'=>$this,'xml_promo_st'=>$xml_promo_st]);
             $xml = array();
