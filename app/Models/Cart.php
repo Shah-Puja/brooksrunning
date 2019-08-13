1ef47d6a429afbf2a53239fc6f3a265223e8c5bb
trunalt <trunaltamore@gmail.com>
2019-08-13 16:12:31 +0530

    new changes

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -111,7 +111,7 @@ class Cart extends Model {
             );
             Ap21_log::createNew($logger);
             if (!empty($xml) && !isset($xml->ErrorCode)) {
-                $cartdetail_arr =collect($xml->CartDetails);
+                $cartdetail_arr =collect($xml->CartDetails->CartDetail);
                 $xml_freight_charges = $xml->SelectedFreightOption->Value; //Freight chareges
                 $total_due = (array) $xml->TotalDue;
                 $cart_total = $total_due[0];
