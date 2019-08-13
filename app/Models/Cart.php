eb2717108abb6abf594d96c3c27b94a54effe96b
trunalt <trunaltamore@gmail.com>
2019-08-13 14:50:38 +0530

    new changes

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -99,7 +99,7 @@ class Cart extends Model {
             $cart_xml = view('xml.cart_call')->with(['cart'=>$this,'xml_promo_st'=>$xml_promo_st]);
             $xml = array();
             $cart_xml_response = $bridgeObject->processCart($cart_xml);
-
+            print_r($cart_xml);
             print_r($cart_xml_response);
             exit;
 
