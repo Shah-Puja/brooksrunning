aeb813e53f7595442264190734c7572129764e6a
trunalt <trunaltamore@gmail.com>
2019-08-13 10:27:07 +0530

    new changes

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -147,7 +147,7 @@ class Cart extends Model {
             $promo_code = promo_mast::where('promo_string', $this->promo_code)->whereRaw('CURDATE() between `start_dt` and `end_dt`')->first();
             if (!empty($promo_code)) {
                 $promo_code->push('qty',1);
-                collect($this)->push($promo_code);
+                $this->cartItems = $promo_code;
                 echo "<pre>";
                 print_r($this);
                 exit;
