768b02ccb3bcb989f98031a41330437de23b61b1
trunalt <trunaltamore@gmail.com>
2019-08-16 11:53:54 +0530

    new changes

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -237,7 +237,7 @@ class Cart extends Model {
             Ap21_log::createNew([
                 'process' =>'Gift voucher',
                 'request' => 'Pin:'.$this->pin.', gift_id:'.$this->gift_id,
-                'response' =>,
+                'response' =>null,
                 'object_id'=>session('cart_id')
             ]);
         }else{
