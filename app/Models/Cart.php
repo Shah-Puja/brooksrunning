49d1701f613bbeffb0fcfbb30d50eeec7df29a24
trunalt <trunaltamore@gmail.com>
2019-08-16 15:06:13 +0530

    new changes

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -113,9 +113,13 @@ class Cart extends Model {
                                     ]);
                     }                                                            
                     $cart_xml_total = $xml->TotalDue;
+                    echo "cart_xml_total".$cart_xml_total;
                     $freight_cost = $this->freight_cost;
+                    echo "freight_cost".$freight_cost;
                     $total_discount = $xml->TotalDiscount;
+                    echo "total_discount".$total_discount;
                     $cart_total = $cart_xml_total - $freight_cost;
+                    echo "cart_total".$cart_total;
                     if(empty($xml_promo_st)){ // update carts if promo data is empty 
                         $this->update([
                             'promo_code' => '', 
