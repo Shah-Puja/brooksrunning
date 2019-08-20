e29e0dd4a011ccb0b1ffea2112fa362efb7b4733
trunalt <trunaltamore@gmail.com>
2019-08-20 16:44:06 +0530

    nova promo code

--- app/Models/Promo_mast.php
+++ app/Models/Promo_mast.php
@@ -7,4 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Promo_mast extends Model
 {
     protected $table = 'promo_mast';
+
+    // protected $fillable = [
+    //     'promo_mast_id','promo_string','promo_code','skuidx','promo_type','promo_title','promo_desc',
+    //     'promo_display_text','start_dt','end_dt','discount','discount_type','active','created_dt'
+    // ];
+    const UPDATED_AT = null;
+    const CREATED_AT = null;
+
+    protected $casts = [
+        'start_dt' => 'datetime',
+        'end_dt' => 'datetime'
+    ];
+
+    protected $primaryKey = 'promo_mast_id';
 }
---
+++ app/Nova/PromoCode.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Nova;
+
+use Laravel\Nova\Fields\ID;
+use Illuminate\Http\Request;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Select;
+use Laravel\Nova\Fields\DateTime;
+
+class PromoCode extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = 'App\Models\Promo_mast';
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'promo_mast_id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'promo_type'
+    ];
+
+
+
+   
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make('Promo id','promo_mast_id')->sortable(),
+            Text::make('Promo string ','promo_string')->sortable()->rules('required'),
+            Text::make('Promo code','promo_code')->sortable()->rules('required'),
+            Text::make('Skuidx','skuidx')->sortable()->rules('required'),
+            
+            Select::make('Promo type','promo_type')->options([
+                'Onetime' => 'Onetime',
+                'Multiple' => 'Multiple',
+            ])->sortable(),
+
+            Text::make('Promo title','promo_title')->sortable()->hideFromIndex(),
+            Text::make('Promo desc','promo_desc')->sortable()->hideFromIndex(),
+            Text::make('Promo Text','promo_display_text')->sortable()->hideFromIndex(),
+            DateTime::make('Start date','start_dt')->sortable()->rules('required'),
+            DateTime::make('End date','end_dt')->sortable()->rules('required'),
+            Text::make('Discount','discount')->sortable(),
+            
+            Select::make('Discount type','discount_type')->options([
+                'percentage' => 'percentage',
+                'flat' => 'flat',
+            ])->sortable(),
+
+            Select::make('Status','active')->options([
+                'Yes' => 'Yes',
+                'No' => 'No',
+            ])->sortable(),
+            
+
+        ];
+    }
+	
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
