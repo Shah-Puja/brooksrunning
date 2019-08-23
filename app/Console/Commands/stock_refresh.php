e1e0a2918febf163d7388def6fc9ebd5b1286634
purvishah <purvi.cshah@gmail.com>
2019-08-23 16:45:18 +0530

    a

--- app/Console/Commands/stock_refresh.php
+++ app/Console/Commands/stock_refresh.php
@@ -43,19 +43,17 @@ class stock_refresh extends Command
     {
         
         /*
-        echo "\n1 Start : ".date('Y-m-d H:i:s');
+        echo "\n0 Start : ".date('Y-m-d H:i:s');
         $prod_xml = $this->bridgeObject->allProducts();
         //$prod_xml = $this->bridgeObject->getProduct('28742');
         Storage::disk('public')->put('ap21product/data.xml', $prod_xml); 
+        echo "\n1 store XML to File : ".date('Y-m-d H:i:s');        
         */
 
-        $xml_response_obj =  Storage::disk('public')->get('ap21product/data.xml');                     
-        //echo "aa : \n ".$xml_response_obj;
-        echo "\n2 Call Over : ".date('Y-m-d H:i:s');
-        //exit;
+        $xml_response_obj =  Storage::disk('public')->get('ap21product/data.xml');                             
+        echo "\n2 Read File : ".date('Y-m-d H:i:s');        
 
-        if (!empty($xml_response_obj)) {
-            //$bridge = $xml_response_obj->getContents();            
+        if (!empty($xml_response_obj)) {            
             echo "\n 3 Got Content : ".date('Y-m-d H:i:s');				            
             $xml = simplexml_load_string($xml_response_obj);
             echo "\n 4 Created array : ".date('Y-m-d H:i:s');		
@@ -67,58 +65,21 @@ class stock_refresh extends Command
                         foreach ($curr_color->SKUs->SKU as $curr_sku){
                             $stock =$curr_sku->FreeStock;
                             $skuidx = $curr_sku->Id;
-                            $records[]=['skuidx'=>$curr_sku->Id,'stock'=>$curr_sku->FreeStock];
-                            //Ap21_stock::create(['skuidx'=>$curr_sku->Id,'stock'=>$curr_sku->FreeStock]);                            
+                            $records[]=['skuidx'=>$curr_sku->Id,'stock'=>$curr_sku->FreeStock];                            
                         }
                     }
                 }
-                //print_r($records);
                 Ap21_stock::insert($records); 
+                echo "\n 5 ap21_stock created ".date('Y-m-d H:i:s');
                 DB::table('p_variants as a')
                         ->join('ap21_stock as b', 'a.id', '=', 'b.skuidx')
                         ->update([ 'a.stock' => DB::raw("`b`.`stock`") ]);
+                echo "\n 6 p_variants updated ".date('Y-m-d H:i:s');
             }            
-            echo "\n 5 Complete ".date('Y-m-d H:i:s');
+            echo "\n 7 Over. ".date('Y-m-d H:i:s');
         }
         else{
             echo "\n xml_response_obj is empty";
-        }
-        
-/*
-
-        echo "\n1 Start : ".date('Y-m-d H:i:s');
-        //$xml_response_obj = $this->bridgeObject->allProducts();
-        $xml_response_obj = $this->bridgeObject->getProduct('206089');
-        echo $xml_response_obj;
-        exit;
-		/*echo "\n2 Call Over : ".date('Y-m-d H:i:s');
-        if ($xml_response_obj){
-            $xml_response = $xml_response_obj->getContents();
-            echo "\n 3 Got Content : ".date('Y-m-d H:i:s');				
-            $xml = simplexml_load_string($xml_response);
-            echo "\n 4 Created array : ".date('Y-m-d H:i:s');		
-            if (!empty($xml) && !isset($xml->ErrorCode)) {
-                //echo $xml->Product
-                foreach ( $xml->Product as $curr_product){
-                    foreach ($curr_product->Clrs->Clr as $curr_color){
-                        foreach ($curr_color->SKUs->SKU as $curr_sku){
-                            $stock =$curr_sku->FreeStock;
-                            $skuidx = $curr_sku->Id;
-                            echo "\n $sku_idx - $stock";
-                            Ap21_stock::create(['skuidx'=>$curr_sku->Id,'stock'=>$curr_sku->FreeStock]);
-                            exit;
-                        }
-                    }
-                }
-            }            
-            echo "\n 5 Complete ".date('Y-m-d H:i:s');
-        }
-        else{
-            echo "Error";
-        
-        }
-        */
-        
-
+        }        
     }
 }
