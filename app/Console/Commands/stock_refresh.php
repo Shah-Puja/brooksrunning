4c4ec70f4fb3e00d64c25287d663dd4c84a0ac9e
purvishah <purvi.cshah@gmail.com>
2019-08-23 14:46:47 +0530

    a

--- app/Console/Commands/stock_refresh.php
+++ app/Console/Commands/stock_refresh.php
@@ -72,8 +72,8 @@ class stock_refresh extends Command
                         }
                     }
                 }
-                print_r($records);
-                Ap21_stock::create($records); 
+                //print_r($records);
+                Ap21_stock::insert($records); 
             }            
             echo "\n 5 Complete ".date('Y-m-d H:i:s');
         }
