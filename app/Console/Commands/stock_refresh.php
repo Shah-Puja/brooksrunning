83e77a0fd2c6f86714f0dbe3fc15d8614042af18
purvishah <purvi.cshah@gmail.com>
2019-08-23 12:44:45 +0530

    a

--- app/Console/Commands/stock_refresh.php
+++ app/Console/Commands/stock_refresh.php
@@ -42,19 +42,31 @@ class stock_refresh extends Command
         echo "\n1 Start : ".date('Y-m-d H:i:s');
         $xml_response_obj = $this->bridgeObject->allProducts();
         //$xml_response_obj = $this->bridgeObject->getProduct('28742');
-        print_r($xml_response_obj);
-        echo "<hr>";
+        //print_r($xml_response_obj);
+        echo "\n2 Call Over : ".date('Y-m-d H:i:s');
+    
         if (!empty($xml_response_obj)) {
             $bridge = $xml_response_obj->getContents();
-            print_r($bridge);
-            echo "<hr>";
+            echo "\n 3 Got Content : ".date('Y-m-d H:i:s');				            
             $xml = simplexml_load_string($bridge);
-            print_r($xml); 
-            echo "<hr>";
-            exit;
+            echo "\n 4 Created array : ".date('Y-m-d H:i:s');		
+            if (!empty($xml) && !isset($xml->ErrorCode)) {
+                foreach ( $xml->Product as $curr_product){
+                    foreach ($curr_product->Clrs->Clr as $curr_color){
+                        foreach ($curr_color->SKUs->SKU as $curr_sku){
+                            $stock =$curr_sku->FreeStock;
+                            $skuidx = $curr_sku->Id;
+                            echo "\n $sku_idx - $stock";
+                            Ap21_stock::create(['skuidx'=>$curr_sku->Id,'stock'=>$curr_sku->FreeStock]);
+                            exit;
+                        }
+                    }
+                }
+            }            
+            echo "\n 5 Complete ".date('Y-m-d H:i:s');
         }
         else{
-            echo "xml_response_obj is empty";
+            echo "\n xml_response_obj is empty";
         }
         
 /*
