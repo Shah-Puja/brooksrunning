4309c11477bff598a40c4f1cbc64b97195c62e6e
purvishah <purvi.cshah@gmail.com>
2019-08-26 11:12:53 +0530

    b

--- app/Payments/AfterpayProcessor.php
+++ app/Payments/AfterpayProcessor.php
@@ -62,8 +62,8 @@ class AfterpayProcessor {
             "email" => $order->address->email
           ], 
           "merchant" => [
-            "redirectConfirmUrl" => config('app.url') . "/afterpay_success",
-            "redirectCancelUrl" => config('app.url') . "/afterpay_cancel"
+            "redirectConfirmUrl" => "/afterpay_success",
+            "redirectCancelUrl" => "/afterpay_cancel"
           ],
           "merchantReference" => $order->id,
           "items" => $order_items,
