e9fce4ceda266f157dd676d3b71b59a1e057d18c
purvishah <purvi.cshah@gmail.com>
2019-09-16 16:04:21 +0530

    test

--- app/SYG/Bridges/AP21Bridge.php
+++ app/SYG/Bridges/AP21Bridge.php
@@ -89,11 +89,15 @@ class AP21Bridge implements BridgeInterface {
                 return $response;
             }
         } catch (RequestException $e) {
+            echo "Catch 1";
+            exit;
             if ($e->getMessage() != '') {
                 Order::ap21_error('Get Person API',$url,$email, $object_id ,$e->getMessage());
                 return null;
             }
         } catch (\Exception $exception) {
+            echo "Catch 2";
+            exit;
             if ($exception->getMessage() != '') {
                 Order::ap21_error('Get Person API',$url,$email, $object_id ,$exception->getMessage());
                 return null;
