259f12528d5f99490716b58acc5b29040ed56ccb
purvishah <purvi.cshah@gmail.com>
2019-09-17 17:57:26 +0530

    a

--- app/Models/Ap21_error.php
+++ app/Models/Ap21_error.php
@@ -19,9 +19,9 @@ class Ap21_error extends Model
         print_r($error_object);
         self::createNew($error_object);
 
-        // Mail::to(config('site.notify_email'))
-        //       ->cc(config('site.syg_notify_email'))
-        //       ->send(new Ap21Alert($error_object));
+         Mail::to(config('site.notify_email'))
+               ->cc(config('site.syg_notify_email'))
+               ->send(new Ap21Alert($error_object));
     }
 }
  
