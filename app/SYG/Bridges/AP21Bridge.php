8babf2a87bb02506314770f94f1205152dab3c19
trunalt <trunaltamore@gmail.com>
2019-09-17 10:43:42 +0530

    new changes

--- app/SYG/Bridges/AP21Bridge.php
+++ app/SYG/Bridges/AP21Bridge.php
@@ -82,8 +82,8 @@ class AP21Bridge implements BridgeInterface {
     }
 
     public function getPersonid($email,$object_id='0') {
-        return $this->apiClient->get('Persons/?countryCode=AUFIT&email=' . $email, ['http_errors' => false]);
-       /* $url='Persons/?countryCode=AUFIT&email=' . $email;        
+        //return $this->apiClient->get('Persons/?countryCode=AUFIT&email=' . $email, ['http_errors' => false]);
+        $url='Persons/?countryCode=AUFIT&email=' . $email;        
         try {
             $response = $this->apiClient->get($url, ['http_errors' => false]);
             if (!empty($response)) {                
@@ -101,7 +101,7 @@ class AP21Bridge implements BridgeInterface {
                 Order::ap21_error('Get Person API',$url,$email, $object_id ,$exception->getMessage());
                 return null;
             }
-        }*/
+        }
     }
 
     public function processPerson($data, $object_id='0') {
