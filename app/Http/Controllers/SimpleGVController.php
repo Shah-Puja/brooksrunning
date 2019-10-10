56bf1788fcc947b4e72df6304554db9d70a46352
trunalt <trunaltamore@gmail.com>
2019-10-10 15:09:31 +0530

    new changes

--- app/Http/Controllers/SimpleGVController.php
+++ app/Http/Controllers/SimpleGVController.php
@@ -14,15 +14,10 @@ class SimpleGVController extends Controller
         if($orders!=''){
             foreach($orders as $order){
                 if($order->giftcert_ap21code!='' && $order->giftcert_ap21pin!=''){
-                    $gif_array[] = [ 'gv' => $order->giftcert_ap21code,'pin' => $order->giftcert_ap21pin ];
+                    SimpleGv::where('gv' ,$order->giftcert_ap21code)
+                            ->where('pin' ,$order->giftcert_ap21pin)
+                            ->update(['used'=>'Yes']);
                 }
-            }
-            print_r($gif_array);
-            if($gif_array!=''){
-                SimpleGv::where(function ($query) use ($gif_array) {
-                    $query->where('gv', $gif_array->gv)
-                          ->where('pin', $gif_array->pin);
-                })->update(['used'=>'Yes']);
             }
         }
    } 
