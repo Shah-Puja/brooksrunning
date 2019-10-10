542339f7d562921975e9ce6f34d5527954c0f0cf
trunalt <trunaltamore@gmail.com>
2019-10-10 15:40:56 +0530

    new changes

--- app/Http/Controllers/SimpleGVController.php
+++ app/Http/Controllers/SimpleGVController.php
@@ -4,22 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\SimpleGv;
-use App\Models\Order;
+use App\SYG\Bridges\BridgeInterface;
 
 class SimpleGVController extends Controller
-{
-   public function gv_check(){
-        $orders = Order::whereIn('status',['Completed','Order Number'])->get();
-        $gif_array = [];
-        if($orders!=''){
-            foreach($orders as $order){
-                if($order->giftcert_ap21code!='' && $order->giftcert_ap21pin!=''){
-                    SimpleGv::where('gv',$order->giftcert_ap21code)
-                            ->where('pin',$order->giftcert_ap21pin)
-                            ->update(['used'=>'Yes']);
-                }
+{  
+    public function __construct(BridgeInterface $bridge)
+	{
+        $this->bridge = $bridge;
+    }
+    public function gv_check(){
+       $simplegv = SimpleGv::where('used','No')->get();
+       if($simplegv!=''){
+            foreach($simplegv as $value){
+                $response=$this->bridge->vouchervalid($value->gv,$value->pin,'100');    
+                $returnCode = $response->getStatusCode();    
+                    switch ($returnCode) {
+                            case 200:        
+                                $value->used="NO";
+                                $value->save();
+                                break;
+                            default:
+                                $value->used="Yes";
+                                $value->save();
+                                break;
+                        } 
             }
-            echo "Process Completed";
-        }
-   } 
+       }
+       
+    } 
 }
