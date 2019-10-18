0f6f8e1aa46412b83c142d9c57c0a0832b5a6e97
[sachintiwari809@gmail.com] <gsundara2014@gmail.com>
2019-10-18 17:02:14 +0530

    new instagram compt addded

---
+++ resources/views/meet_brooks/terms_conditions/trickortreat_ghost12_instagram_terms.blade.php
@@ -0,0 +1,35 @@
+<div class="privacy-content">
+  <p><strong><u>Trick or Treat? Ghost 12 Instagram Competition Terms &amp; Conditions</u></strong></p>
+<ol>
+    <li>Information on &quot;How to Enter&quot; and prizes form part of these Conditions of Entry.</li>
+    <li>Entry is open to Australian and New Zealand residents, except employees of Texas Peak Pty Ltd, Bocara Limited and their immediate families.</li> 
+    <li>The entire promotional period for the NSW trade promotion lottery permit commences on Wednesday 21st November 2018 and concludes Wednesday 20th November 2019. </li>
+    <li>The entire Promotional period for this competition commences on Wednesday 30th October and concludes Thursday 31st October 2019 at 11:59am. The winners will be selected on Thursday 31st October 2019 at 12:00pm AEST on the premises of Texas Peak, 30 Tullamarine Park Rd, Tullamarine, Vic, <i>Australia</i>. The winner will be notified by a direct message to their Instagram account and on the Brooks Running Australia Instagram story. </li>
+    <li>To enter via Instagram participants must complete the following in the promotional period:
+        <ol style="margin-left: 0px; margin-top:15px; margin-bottom: 15px;">
+              <li>Like the competition post</li>
+              <li>Tag a friend</li>
+              <li>Follow @brooksrunningau</li>
+        </ol>
+    </li> 
+    <li>By entering and participating, entrant agrees to exclude Instagram from and against all claims. This promotion is in no way sponsored, endorsed or administered, or associated with Instagram. </li>
+    <li>Entries not completed in accordance with the Conditions of Entry or received after close of entry will not be considered. Incomplete or incorrect entries will be disregarded. It is the participant’s responsibility to ensure all details are full and correct. The Promoter reserves the right to verify the validity of entries and reserves the right to disqualify any entrant for tampering with the entry process. The judge&apos;s decision in relation to any aspect of the Promotion is final and binding on every person who enters. </li>
+    <li>The winners of the Promotion will be chosen at random. The prizes must be claimed before 5pm AEST on 21st November 2019 otherwise a redraw will occur. If in the event the prize is not claimed a redraw will occur on the 22st November 2019 at 12:00pm AEST at the premises of Texas Peak. </li>
+    <li>Prizes are as follows:
+        <ul style="margin-left: 35px; margin-top:15px; margin-bottom: 15px;">
+              <li> -  1 x Brooks Ghost 12 </li>
+            
+        </ul>
+    </li>
+    <li>In conjunction with permit regulations, for any promotion conducted no individual prize will exceed $1,000 AUD and the total value of prizes will not exceed $100,000 AUD. NSW Permit No. LTPM/18/03786.     </li>
+    <li>The Promoter reserves the right to request winners to provide proof of their identity and proof of Australian or New Zealand residency in order to claim the prize. Proof of identification and residency is at the judgment of the promoter. The Promoter reserves the right to ask for kms and time of mapped run to determine the entry is not fraudulent. </li>
+    <li>Entrants must be over 18 years of age, or have permission from their Parent or Guardian.</li>
+    <li>All prizes must be taken as offered. The prize, or any unused portion of the prize, is not transferable or exchangeable and cannot be taken as cash. The Promoter accepts no responsibility for any variation in the prize value.</li>
+  <li>If for any reason this competition is not capable of running as planned (including but not limited to) infection by computer virus, bugs, tampering, unauthorised intervention, fraud, technical failures or any other causes beyond the control of the Promoter which corrupts or affects the administration security, fairness, integrity or proper conduct of this competition, the Promoter reserves the right in its sole discretion to cancel, terminate, modify or suspend the competition in accordance with all state and territory regulations.</li>
+   <li>The Promoter reserves the right in its sole discretion to disqualify any individual who tampers with the entry process. All entries become the property of the Promoter.</li>
+   <li>The Promoter may use information it collects in relation to you, to provide you with information regarding further offers, promotions or goods and services provided by the Promoter and/or the Promoter&apos;s partners and sponsors. To remove your information from the Promoter’s records, send an email to <a href="mailto:info@brooksrunning.com.au" target="_top">info@brooksrunning.com.au</a> with your name, phone number and your request to cancel the Service. To view the Promoters Privacy Policy please visit <a href="https://www.brooksrunning.com.au/info/privacy">https://www.brooksrunning.com.au/info/privacy</a>.</li>
+   <li>The Promoter is not responsible for any error, omission, destruction or unauthorised access to, or alteration of entries occurring in relation to the conduct of the Promotion.</li>
+   <li>To the full extent permitted by law, the Promoter will not be liable to you in respect of any claim for any loss or damage whatsoever (including, without limitation, any consequential, indirect, special, punitive or incidental damages) or for any personal injury suffered by you as a result of or in connection with the Promotion.</li>
+  <li>The Promoter is Texas Peak Pty Ltd, 30 Tullamarine Park Rd, Tullamarine VIC 3043. ABN 70 055 241 248</li>
+</ol>
+   </div>
