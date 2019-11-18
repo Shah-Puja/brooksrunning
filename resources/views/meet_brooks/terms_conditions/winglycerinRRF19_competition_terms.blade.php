a1a1dbf1f097c0f052a6e682ba022cc913f43af2
Arati Mapankar <aaru.mapankar@gmail.com>
2019-11-18 17:38:05 +0530

    Terms and conditions for rrf2019

--- resources/views/meet_brooks/terms_conditions/winglycerinRRF19_competition_terms.blade.php
+++ resources/views/meet_brooks/terms_conditions/winglycerinRRF19_competition_terms.blade.php
@@ -1,28 +1,27 @@
 <div class="privacy-content">
-  <p><strong><u>Win an Adrenaline GTS 20 Competition Terms &amp; Conditions</u></strong></p>
+  <p><strong><u>Rotorua Running Festival 2019 Competition Terms &amp; Conditions</u></strong></p>
 <ol>
-    <li>Information on &quot;How to Enter&quot; and prizes form part of these Conditions of Entry.</li>
-    <li>Entry is open to Victorian, Queensland, Australian Capital Territory, South Australian, Northern Territory, Tasmanian, New South Wales or Western Australian residents, except employees of Fit Chain Pty Ltd and their immediate families.</li>
-    <li>The entire promotional period for the NSW trade promotion lottery permit commences on November 2018 and concludes Wednesday 20th November 2019. </li>
-    <li>The entire Promotional period for this competition commences on Monday 4th November and concludes Tuesday 19th November. The winner will be selected on Wednesday 20th November at 12:00pm AEST on the premises of Fit Chain, 30 Tullamarine Park Rd, Tullamarine VIC, <i>Australia</i>. The winner will be notified by email.</li>
-    <li>To enter participants must complete a competition entry form at <a href="https://www.brooksrunning.com.au/meet_brooks/competition/rw_competition">https://www.brooksrunning.com.au/meet_brooks/competition/rw_competition</a>  during the dates specified above. It is the participant&apos;s responsibility to ensure all details are full and correct before submitting their entry form.</li> 
-    <li>Entries not completed in accordance with the Conditions of Entry or received after close of entry will not be considered. Incomplete or incorrect entries will be disregarded. The Promoter reserves the right to verify the validity of entries and reserves the right to disqualify any entrant for tampering with the entry process. The judge&apos;s decision in relation to any aspect of the Promotion is final and binding on every person who enters. </li>
-    <li>The winner of the Promotion will be chosen at random. The prizes must be claimed before 5pm AEST on 25th November 2019 otherwise a redraw will occur. If in the event the prize is not claimed, a redraw will occur on the 26th November at 12:00pm AEST at the premises of Fit Chain. The winner will be notified by email.</li>
+    <li>Information on "How to Enter" and prizes form part of these Conditions of Entry.</li>
+    <li>Entry is open to New Zealand residents, except employees of Texas Peak Pty Ltd or Bocara Limited and their immediate families.</li>
+    <li>The entire Promotional period commences on Tuesday 19th November and closes on Sunday 1st December 2019. The winner will be selected on Monday 2nd December 2019 at 12:00pm AEST on the premises of Texas Peak, 30 Tullamarine Park Rd, Tullamarine, Vic, Australia. The winner will be notified by email.</li>
+    <li>To enter participants must complete a competition entry form at https://www.brooksrunning.com.au/meet_brooks/competition/winglycerinRRF19 during the dates specified above. It is the participant’s responsibility to ensure all details are full and correct before submitting their entry form.</li>
+    <li>Entries not completed in accordance with the Conditions of Entry or received after close of entry will not be considered. Incomplete or incorrect entries will be disregarded. The Promoter reserves the right to verify the validity of entries and reserves the right to disqualify any entrant for tampering with the entry process. The judge’s decision in relation to any aspect of the Promotion is final and binding on every person who enters.</li>
+    <li>The winners of the Promotion will be chosen at random. The prizes must be claimed before 12pm AEST on the 16th December 2019 otherwise a redraw will occur. If in the event the prize is not claimed a redraw will occur on the 17th  December at 2:00pm AEST at the premises of Texas Peak. The winner will be notified by email.</li>
     <li>Each prize is as follows:
         <ul style="margin-left: 35px; margin-top:15px; margin-bottom: 15px;">
-              <li>2 x Brooks Adrenaline GTS 20 (RRP $219.95 each)</li>
+              <li>A pair of Brooks Glycerin 17  (RRP $299.00 NZD)</li>
             
         </ul>
     </li>
-    <li>In conjunction with permit regulations, for any promotion conducted no individual prize will exceed $1,000 AUD and the total value of prizes will not exceed $100,000 AUD. NSW Permit No. LTPM/18/03786.    </li>
-    <li>The Promoter reserves the right to request the winner to provide proof of their identity and proof of Australian residency in order to claim the prize. Proof of identification and residency is at the judgment of the promoter.</li>
+    <li>The Promoter reserves the right to request winners to provide proof of their identity and proof of Australian or New Zealand residency in order to claim the prize. Proof of identification and residency is at the judgment of the promoter.</li>
     <li>Entrants must be over 18 years of age, or have permission from their Parent or Guardian.</li>
     <li>All prizes must be taken as offered. The prize, or any unused portion of the prize, is not transferable or exchangeable and cannot be taken as cash. The Promoter accepts no responsibility for any variation in the prize value.</li>
     <li>If for any reason this competition is not capable of running as planned (including but not limited to) infection by computer virus, bugs, tampering, unauthorised intervention, fraud, technical failures or any other causes beyond the control of the Promoter which corrupts or affects the administration security, fairness, integrity or proper conduct of this competition, the Promoter reserves the right in its sole discretion to cancel, terminate, modify or suspend the competition in accordance with all state and territory regulations.</li>
-   <li>The Promoter reserves the right in its sole discretion to disqualify any individual who tampers with the entry process. All entries become the property of the Promoter.</li>
-   <li>The Promoter may use information it collects in relation to you, to provide you with information regarding further offers, promotions or goods and services provided by the Promoter and/or the Promoter&apos;s partners and sponsors. To remove your information from the Promoter&apos;s records, send an email to <a href="mailto:info@brooksrunning.com.au" target="_top">info@brooksrunning.com.au</a> with your name, phone number and your request to cancel the Service. To view the Promoters Privacy Policy please visit <a href="https://www.brooksrunning.com.au/info/termsandconditions">http://www.brooksrunning.com.au/privacy_policy.php</a>.</li>
-   <li>The Promoter is not responsible for any error, omission, destruction or unauthorised access to, or alteration of entries occurring in relation to the conduct of the Promotion.</li>
-   <li>To the full extent permitted by law, the Promoter will not be liable to you in respect of any claim for any loss or damage whatsoever (including, without limitation, any consequential, indirect, special, punitive or incidental damages) or for any personal injury suffered by you as a result of or in connection with the Promotion.</li>
-  <li>The Promoter is Fit Chain Pty Ltd, 30 Tullamarine Park Rd, Tullamarine VIC 3043. ABN 46125569039</li>
+    <li>The Promoter reserves the right in its sole discretion to disqualify any individual who tampers with the entry process. All entries become the property of the Promoter.</li>
+    <li>The Promoter may use information it collects in relation to you, to provide you with information regarding further offers, promotions or goods and services provided by the Promoter and/or the Promoter’s partners and sponsors. To remove your information from the Promoter’s records, send an email to info@brooksrunning.com.au with your name, phone number and your request to cancel the Service. To view the Promoters Privacy Policy please visit https://www.brooksrunning.com.au/info/privacy</li>
+    <li>The Promoter is not responsible for any error, omission, destruction or unauthorised access to, or alteration of entries occurring in relation to the conduct of the Promotion.</li>
+    <li>To the full extent permitted by law, the Promoter will not be liable to you in respect of any claim for any loss or damage whatsoever (including, without limitation, any consequential, indirect, special, punitive or incidental damages) or for any personal injury suffered by you as a result of or in connection with the Promotion.</li>
+    <li>The Promoter is Texas Peak Pty Ltd, 30 Tullamarine Park Rd, Tullamarine VIC 3043. ABN 70 055 241 248</li>
+</li>
 </ol>
    </div>
